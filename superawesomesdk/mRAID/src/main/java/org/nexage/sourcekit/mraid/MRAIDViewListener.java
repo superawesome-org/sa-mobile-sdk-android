f40eebef894acbddd2b2393fd506685f969128d5
Connor Leigh-Smith <connor.leigh-smith@superawesome.tv>
2015-07-07 18:28:06 +0100

    Revert "Adding mRAID repo as submodule"
    This reverts commit c46d2924cf49dad44ba3e9ae500dc6f2aea51b8a.

---
+++ superawesomesdk/mRAID/src/main/java/org/nexage/sourcekit/mraid/MRAIDViewListener.java
@@ -0,0 +1,22 @@
+package org.nexage.sourcekit.mraid;
+
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.ImageButton;
+
+public interface MRAIDViewListener {
+
+    /******************************************************************************
+     * A listener for basic MRAIDView banner ad functionality.
+     ******************************************************************************/
+
+    public void mraidViewLoaded(MRAIDView mraidView);
+
+    public void mraidViewExpand(MRAIDView mraidView);
+
+    public void mraidViewClose(MRAIDView mraidView);
+
+    public boolean mraidViewResize(MRAIDView mraidView, int width, int height, int offsetX, int offsetY);
+
+    public void mraidViewLayoutChange(View view);
+}
