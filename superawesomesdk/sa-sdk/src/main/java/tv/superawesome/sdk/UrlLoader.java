5fd497194f9f607324911b86984a8cdd8d0f1021
Connor Leigh-Smith <connor.leigh-smith@superawesome.tv>
2015-09-03 17:43:30 +0100

    Swiched back to https but not checking SSL cert (need to fix)

--- superawesomesdk/sa-sdk/src/main/java/tv/superawesome/sdk/UrlLoader.java
+++ superawesomesdk/sa-sdk/src/main/java/tv/superawesome/sdk/UrlLoader.java
@@ -9,6 +9,13 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 /**
  * Created by connor.leigh-smith on 24/06/15.
@@ -23,14 +30,29 @@ public class UrlLoader extends AsyncTask<String, Integer, String> {
 
     @Override
     protected String doInBackground(String[] params) {
-        HttpURLConnection urlConnection = null;
+        HttpsURLConnection urlConnection = null;
         InputStream in = null;
         BufferedReader streamReader = null;
         StringBuilder responseStrBuilder = new StringBuilder();
 
         try {
-            urlConnection = (HttpURLConnection) new URL(params[0]).openConnection();
-        } catch (IOException e) {
+            TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+                public X509Certificate[] getAcceptedIssuers() {
+                    return null;
+                }
+                @Override
+                public void checkClientTrusted(X509Certificate[] arg0, String arg1) throws CertificateException {
+                }
+                @Override
+                public void checkServerTrusted(X509Certificate[] arg0, String arg1) throws CertificateException {
+                }
+            } };
+            SSLContext sc = SSLContext.getInstance("TLS");
+            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+
+            urlConnection = (HttpsURLConnection) new URL(params[0]).openConnection();
+        } catch (Exception e) {
             listener.onBeginLoad(e.getMessage());
         }
 
