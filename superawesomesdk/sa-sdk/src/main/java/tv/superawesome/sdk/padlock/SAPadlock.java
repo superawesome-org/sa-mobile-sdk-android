93b04fd9cf58eccf7473a542026197493178df43
Gabriel Coman <dev.gabriel.coman@gmail.com>
2015-09-18 15:49:38 +0100

    new inflater approach in sapadlock

--- superawesomesdk/sa-sdk/src/main/java/tv/superawesome/sdk/padlock/SAPadlock.java
+++ superawesomesdk/sa-sdk/src/main/java/tv/superawesome/sdk/padlock/SAPadlock.java
@@ -6,6 +6,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
 import android.widget.TextView;
 import android.os.Bundle;
 import android.view.View;
@@ -26,31 +27,61 @@ public class SAPadlock{
     }
 
     public void createPadlock(final Context context) {
-        /* What should happen when the padlock is tapped? */
-        AlertDialog.Builder builder = new AlertDialog.Builder(context, R.style.Base_Theme_AppCompat_Light_Dialog_Alert);
+        LayoutInflater li = LayoutInflater.from(context);
+        View v = li.inflate(R.layout.dialog_padlock, null);
 
-        // create the view
-//        View v = View.inflate(context, R.layout.dialog_padlock, null);
+        TextView t = (TextView) v.findViewById(R.id.clickHereTextView);
+        t.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+            }
+        });
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(context, R.style.Base_Theme_AppCompat_Light_Dialog_Alert);
+        builder.setView(v);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // do nothing
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+
+//        /* What should happen when the padlock is tapped? */
+//        AlertDialog.Builder builder = new AlertDialog.Builder(context, R.style.Base_Theme_AppCompat_Light_Dialog_Alert);
 //
-//        // get the text view and add listeners
-//        TextView t = (TextView)v.findViewById(R.id.clickHereTextView);
+//        // create the view
+////        View v = View.inflate(context, R.layout.dialog_padlock, null);
+////
+////        // get the text view and add listeners
+////        TextView t = (TextView)v.findViewById(R.id.clickHereTextView);
+//
+//
+//        // set view and other builder stuff
+//        builder.setView(R.layout.dialog_padlock);
+//        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+//            public void onClick(DialogInterface dialog, int id) {
+////                 User clicked OK button
+//            }
+//        });
+//
+//        // create the dialog and show the dialog
+//        AlertDialog dialog = builder.create();
+//
+//        LayoutInflater inflater = dialog.getLayoutInflater();
+//        View v = inflater.inflate(R.layout.dialog_padlock, null);
+//        TextView t = (TextView) v.findViewById(R.id.clickHereTextView);
+//
 //        t.setOnClickListener(new OnClickListener() {
 //            @Override
 //            public void onClick(View v) {
 //                context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
 //            }
 //        });
-
-        // set view and other builder stuff
-        builder.setView(R.layout.dialog_padlock);
-        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int id) {
-                // User clicked OK button
-            }
-        });
-
-        // create the dialog and show the dialog
-        AlertDialog dialog = builder.create();
-        dialog.show();
+//
+//        dialog.show();
     }
 }
