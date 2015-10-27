562cfb3ababeb031153961da11167309c2e6cfe6
Gabriel Coman <dev.gabriel.coman@gmail.com>
2015-10-27 17:49:41 +0000

    Add MoPub adaptors

---
+++ superawesomesdk/sa-sdk/mopub/SABannerViewCustomEvent.java
@@ -0,0 +1,80 @@
+package tv.superawesome.mopub;
+
+import android.content.Context;
+import com.mopub.mobileads.CustomEventBanner;
+import com.mopub.mobileads.MoPubErrorCode;
+
+import java.util.Map;
+import tv.superawesome.sdk.AdManager;
+import tv.superawesome.sdk.SuperAwesome;
+import tv.superawesome.sdk.models.SAAd;
+import tv.superawesome.sdk.views.SABannerView;
+import tv.superawesome.sdk.views.SAPlacementListener;
+
+/**
+ * Created by gabriel.coman on 27/10/15.
+ */
+public class SABannerViewCustomEvent extends CustomEventBanner{
+
+    // private vars
+    private SABannerView banner;
+    private AdManager manager;
+
+    // constructor
+    public SABannerViewCustomEvent(){
+
+    }
+
+    @Override
+    protected void loadBanner(Context context, final CustomEventBannerListener customEventBannerListener, Map<String, Object> map, Map<String, String> map1) {
+
+        // get ad manager
+        manager = SuperAwesome.createAdManager();
+
+        // get map variables
+        String placementId = "";
+        Boolean testMode = true;
+        Boolean isParentalGateEnabled = true;
+
+        if (map1.get("placementId") != null ){
+            placementId = (String)map1.get("placementId").toString();
+        }
+        if (map1.get("testMode") != null) {
+            testMode = Boolean.valueOf(map1.get("testMode"));
+        }
+        if (map1.get("parentalGateEnabled") != null){
+            isParentalGateEnabled = Boolean.valueOf(map1.get("parentalGateEnabled"));
+        }
+
+        banner = new SABannerView(context, placementId, manager);
+        banner.setTestMode(testMode);
+        banner.setParentalGateEnabled(isParentalGateEnabled);
+        banner.loadAd();
+        banner.setListener(new SAPlacementListener() {
+            @Override
+            public void onAdLoaded(SAAd saAd) {
+                System.out.println("Ad fas fetched OK");
+
+                // call to event listener
+                if (customEventBannerListener != null){
+                    customEventBannerListener.onBannerLoaded(banner);
+                }
+            }
+
+            @Override
+            public void onAdError(String s) {
+                System.out.println("Ad fas fetched NOK");
+
+                // call to event listener
+                if (customEventBannerListener != null) {
+                    customEventBannerListener.onBannerFailed(MoPubErrorCode.valueOf("SA Ad Error"));
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onInvalidate() {
+
+    }
+}
