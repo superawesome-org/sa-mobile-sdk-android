30c9448eea5c805fbe7c7551925f283b34a1249f
Gabriel Coman <dev.gabriel.coman@gmail.com>
2015-12-01 16:55:05 +0000

    Updated

--- superawesomesdk/sa-sdk/src/main/java/tv/superawesome/sdk/SuperAwesome.java
+++ superawesomesdk/sa-sdk/src/main/java/tv/superawesome/sdk/SuperAwesome.java
@@ -5,12 +5,21 @@ import android.util.Log;
 public class SuperAwesome {
 
 	private static final String TAG = "SuperAwesome SDK";
-	private static final String VERSION = "2.2.0";
+	private static final String VERSION = "2.2.1";
+	private static final String PLATFORM = "android";
 
 	protected static final String baseUrl = "https://ads.superawesome.tv/v2";
 
 	public static String getVersion(){
 		return VERSION;
+	}
+
+	public static String getPlatform() {
+		return PLATFORM;
+	}
+
+	public static String getSdkVersion() {
+		return SuperAwesome.getPlatform() + "_" + SuperAwesome.getVersion();
 	}
 
 	public SuperAwesome(){
