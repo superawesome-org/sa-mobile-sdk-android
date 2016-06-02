5c59a9f14f0d29a62d4f7b1d5ae1cedf524283ec
Gabriel Coman <dev.gabriel.coman@gmail.com>
2016-06-02 14:42:16 +0100

    3.8.0

--- superawesomesdk/sa-sdk/src/main/java/tv/superawesome/lib/saevents/SAEvents.java
+++ superawesomesdk/sa-sdk/src/main/java/tv/superawesome/lib/saevents/SAEvents.java
@@ -7,9 +7,12 @@ import android.widget.VideoView;
 
 import org.json.JSONObject;
 
+import java.util.HashMap;
+
 import tv.superawesome.lib.sautils.SANetworkInterface;
 import tv.superawesome.lib.sautils.SANetwork;
 import tv.superawesome.lib.sautils.SAUtils;
+import tv.superawesome.samoat.SAMoatEvents;
 import tv.superawesome.sdk.models.SAAd;
 
 /**
@@ -53,17 +56,33 @@ public class SAEvents {
     }
 
     public static void sendDisplayMoatEvent(Activity activity, View view, SAAd ad) {
-        if (!SAUtils.isClassAvailable("com.moat.analytics.mobile.MoatFactory")) return;
-        SAMoatEvents.getInstance().sendDisplayMoatEvent(activity, view, ad);
+        if (!SAUtils.isClassAvailable("tv.superawesome.samoat.SAMoatEvents")) return;
+
+        HashMap<String, String> adIds = new HashMap<String, String>();
+        adIds.put("campaignId", "" + ad.campaignId);
+        adIds.put("lineItemId", "" + ad.lineItemId);
+        adIds.put("creativeId", "" + ad.creative.id);
+        adIds.put("app", "" + ad.app);
+        adIds.put("placementId", "" + ad.placementId);
+
+        SAMoatEvents.getInstance().sendDisplayMoatEvent(activity, view, adIds);
     }
 
     public static void sendVideoMoatEvent(Activity activity, VideoView video, MediaPlayer mp, SAAd ad){
-        if (!SAUtils.isClassAvailable("com.moat.analytics.mobile.MoatFactory")) return;
-        SAMoatEvents.getInstance().sendVideoMoatEvent(activity, video, mp, ad);
+        if (!SAUtils.isClassAvailable("tv.superawesome.samoat.SAMoatEvents")) return;
+
+        HashMap<String, String> adIds = new HashMap<String, String>();
+        adIds.put("campaignId", "" + ad.campaignId);
+        adIds.put("lineItemId", "" + ad.lineItemId);
+        adIds.put("creativeId", "" + ad.creative.id);
+        adIds.put("app", "" + ad.app);
+        adIds.put("placementId", "" + ad.placementId);
+
+        SAMoatEvents.getInstance().sendVideoMoatEvent(activity, video, mp, adIds);
     }
 
     public static void sendVideoMoatComplete(SAAd ad) {
-        if (!SAUtils.isClassAvailable("com.moat.analytics.mobile.MoatFactory")) return;
-        SAMoatEvents.getInstance().sendVideoMoatComplete(ad);
+        if (!SAUtils.isClassAvailable("tv.superawesome.samoat.SAMoatEvents")) return;
+        SAMoatEvents.getInstance().sendVideoMoatComplete(""+ad.placementId);
     }
 }
