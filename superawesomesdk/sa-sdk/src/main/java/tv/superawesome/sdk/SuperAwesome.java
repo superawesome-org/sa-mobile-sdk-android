fabb42dcc9791349eb1e8d3de868ef4fa97d1254
Gabriel Coman <dev.gabriel.coman@gmail.com>
2016-06-27 12:29:55 +0100

    3.9.9

--- superawesomesdk/sa-sdk/src/main/java/tv/superawesome/sdk/SuperAwesome.java
+++ superawesomesdk/sa-sdk/src/main/java/tv/superawesome/sdk/SuperAwesome.java
@@ -55,7 +55,7 @@ public class SuperAwesome {
 
     /** provide versionin */
     private String getVersion () {
-        return "3.9.8";
+        return "3.9.9";
     }
 
     private String getSdk() {
