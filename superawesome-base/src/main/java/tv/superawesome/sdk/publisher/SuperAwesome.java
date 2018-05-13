e15ea16d44f137cd487bb955f47108c91062eb13
Gabriel Coman <dev.gabriel.coman@gmail.com>
2018-05-13 12:02:37 +0100

    Add this

--- superawesome-base/src/main/java/tv/superawesome/sdk/publisher/SuperAwesome.java
+++ superawesome-base/src/main/java/tv/superawesome/sdk/publisher/SuperAwesome.java
@@ -1,9 +1,12 @@
 package tv.superawesome.sdk.publisher;
 
 import android.app.Application;
+import android.content.Context;
 
 import tv.superawesome.lib.saevents.SAEvents;
 import tv.superawesome.lib.sanetwork.file.SAFileDownloader;
+import tv.superawesome.sagdprisminorsdk.minor.SAAgeCheck;
+import tv.superawesome.sagdprisminorsdk.minor.process.GetIsMinorInterface;
 
 /**
  * Created by gabriel.coman on 30/04/2018.
@@ -14,5 +17,9 @@ public class SuperAwesome {
     public static void init (Application application, boolean loggingEnabled) {
         SAEvents.initMoat(application, loggingEnabled);
         SAFileDownloader.cleanup(application);
+    }
+
+    public static void triggerAgeCheck (Context context, String dateOfBirth, GetIsMinorInterface listener) {
+        SAAgeCheck.sdk.getIsMinor(context, dateOfBirth, listener);
     }
 }
