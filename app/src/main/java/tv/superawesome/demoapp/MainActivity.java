c68d1138a1121400d5de4a65bd27d0d3ee1c0dcb
Gabriel Coman <dev.gabriel.coman@gmail.com>
2018-05-21 11:12:02 +0200

    Update

--- app/src/main/java/tv/superawesome/demoapp/MainActivity.java
+++ app/src/main/java/tv/superawesome/demoapp/MainActivity.java
@@ -41,7 +41,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        AwesomeAds.triggerAgeCheck(this, "2012", new GetIsMinorInterface() {
+        AwesomeAds.triggerAgeCheck(this, "2012-02-02", new GetIsMinorInterface() {
             @Override
             public void getIsMinorData(GetIsMinorModel getIsMinorModel) {
                 Log.d("SuperAwesome", "Get is minor " + getIsMinorModel.isMinor + " | " + getIsMinorModel.age);
