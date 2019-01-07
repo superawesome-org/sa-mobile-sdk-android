cf065bdaac321fc7f06be4be3c67c47f8802e14c
Gabriel Coman <dev.gabriel.coman@gmail.com>
2019-01-07 19:58:42 +0000

    Update

--- superawesome-base/src/main/java/tv/superawesome/sdk/publisher/SAVersion.java
+++ superawesome-base/src/main/java/tv/superawesome/sdk/publisher/SAVersion.java
@@ -3,7 +3,7 @@ package tv.superawesome.sdk.publisher;
 public class SAVersion {
 
     // version & sdk private vars
-    private static String version = "7.0.2";
+    private static String version = "7.1.0-beta";
     private static String sdk = "android";
 
     /**
