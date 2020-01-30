3351a2885249d4ca1f306d0d41c6a71e7588b69e
Gabriel Coman <dev.gabriel.coman@gmail.com>
2020-01-30 12:11:28 +0000

    fix(5.2.4): pushed new version out w/ KSF fixes

--- superawesome-base/src/main/java/tv/superawesome/sdk/publisher/SAVersion.java
+++ superawesome-base/src/main/java/tv/superawesome/sdk/publisher/SAVersion.java
@@ -3,7 +3,7 @@ package tv.superawesome.sdk.publisher;
 public class SAVersion {
 
     // version & sdk private vars
-    private static String version = "7.2.3";
+    private static String version = "7.2.4";
     private static String sdk = "android";
 
     /**
