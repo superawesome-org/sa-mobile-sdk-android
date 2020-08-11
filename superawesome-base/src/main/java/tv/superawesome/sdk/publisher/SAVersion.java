4d9287ef06e3c53679310e18003219ba66a6eb50
Gunhan Sancar <cgnhnc@gmail.com>
2020-08-11 21:08:19 +0100

    Update version to 7.2.13

--- superawesome-base/src/main/java/tv/superawesome/sdk/publisher/SAVersion.java
+++ superawesome-base/src/main/java/tv/superawesome/sdk/publisher/SAVersion.java
@@ -3,7 +3,7 @@ package tv.superawesome.sdk.publisher;
 public class SAVersion {
 
     // version & sdk private vars
-    private static String version = "7.2.12";
+    private static String version = "7.2.13";
     private static String sdk = "android";
 
     /**
