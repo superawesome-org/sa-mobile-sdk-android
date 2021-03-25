edad1308f03ff436ac8527e1ba810976db98c5f6
Ariel Adamczyk <arieladamczyk@C02F61WFMD6P.local>
2021-03-25 16:49:39 +0000

    Fixes (with Nicolas and Gabriel) - gradle, close button and layout.

--- superawesome-base/src/main/java/tv/superawesome/sdk/publisher/managed/SAManagedInterstitialAd.kt
+++ superawesome-base/src/main/java/tv/superawesome/sdk/publisher/managed/SAManagedInterstitialAd.kt
@@ -3,13 +3,36 @@ package tv.superawesome.sdk.publisher.managed
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
+import android.view.View
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.RelativeLayout
+import tv.superawesome.lib.sautils.SAImageUtils
+import tv.superawesome.lib.sautils.SAUtils
 
 class SAManagedInterstitialAd: Activity() {
 
     private val banner by lazy {
         SAManagedBannerAd(this)
+    }
+
+    private val closeButton by lazy {
+        // create the close button
+        val fp = SAUtils.getScaleFactor(this)
+        val closeButton = ImageButton(this)
+        closeButton.setVisibility(View.VISIBLE)
+        closeButton.setImageBitmap(SAImageUtils.createCloseButtonBitmap())
+        closeButton.setBackgroundColor(Color.TRANSPARENT)
+        closeButton.setPadding(0, 0, 0, 0)
+        closeButton.scaleType = ImageView.ScaleType.FIT_XY
+        val buttonLayout = RelativeLayout.LayoutParams((30 * fp).toInt(), (30 * fp).toInt())
+        buttonLayout.addRule(RelativeLayout.ALIGN_PARENT_RIGHT)
+        buttonLayout.addRule(RelativeLayout.ALIGN_PARENT_TOP)
+        closeButton.layoutParams = buttonLayout
+        closeButton.setOnClickListener { close() }
+        return@lazy closeButton
     }
 
     private val placementId by lazy {
@@ -20,7 +43,10 @@ class SAManagedInterstitialAd: Activity() {
         super.onCreate(savedInstanceState)
         setContentView(banner)
         banner.load(placementId = placementId)
+        banner.addView(closeButton)
     }
+
+    private fun close() = finish()
 
     companion object {
 
