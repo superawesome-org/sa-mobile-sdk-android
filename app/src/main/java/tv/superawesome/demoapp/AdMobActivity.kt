5fb248815bd00d39d0fab778b8f70049fbc55c2d
mark gilchrist <mark.s.w.gilchrist@gmail.com>
2021-05-13 13:45:07 +0100

    update to latest lib versions

--- app/src/main/java/tv/superawesome/demoapp/AdMobActivity.kt
+++ app/src/main/java/tv/superawesome/demoapp/AdMobActivity.kt
@@ -4,11 +4,17 @@ import android.app.Activity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import com.google.android.gms.ads.*
-import com.google.android.gms.ads.reward.RewardItem
-import com.google.android.gms.ads.reward.RewardedVideoAd
-import com.google.android.gms.ads.reward.RewardedVideoAdListener
-import tv.superawesome.lib.sasession.defines.SAConfiguration
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.LoadAdError
+import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.RequestConfiguration
+import com.google.android.gms.ads.admanager.AdManagerAdRequest
+import com.google.android.gms.ads.admanager.AdManagerInterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
+import com.google.android.gms.ads.rewarded.RewardedAd
+import com.google.android.gms.ads.rewarded.RewardedAdLoadCallback
 import tv.superawesome.plugins.publisher.admob.SAAdMobBannerCustomEvent
 import tv.superawesome.plugins.publisher.admob.SAAdMobExtras
 import tv.superawesome.plugins.publisher.admob.SAAdMobInterstitialCustomEvent
@@ -16,166 +22,97 @@ import tv.superawesome.plugins.publisher.admob.SAAdMobVideoMediationAdapter
 import tv.superawesome.sdk.publisher.SAOrientation
 
 class AdMobActivity : Activity() {
+    private val tag = "SADefaults/AdMob"
     private lateinit var adView: AdView
-    private lateinit var interstitialAd: InterstitialAd
-    private lateinit var rewardedVideoAd: RewardedVideoAd
-    private val configuration = SAConfiguration.PRODUCTION
+    private var interstitialAd: InterstitialAd? = null
+    private var rewardedAd: RewardedAd? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admob)
 
-        MobileAds.initialize(this) { Log.d("SADefaults/Admob", "onInitializationComplete") }
+        MobileAds.initialize(this) { Log.d(tag, "onInitializationComplete") }
+        MobileAds.setRequestConfiguration(
+            RequestConfiguration.Builder()
+                .setTestDeviceIds(listOf("A86A81848A15ED8C5E27A1A72B263929")).build()
+        )
 
-        configureBannerAd()
-        configureInterstitialAd()
-        configureRewardedVideoAd()
-
-        requestBannerAd()
-        requestInterstitialAd()
-        requestVideoAd()
-    }
-
-    private fun configureRewardedVideoAd() {
-        rewardedVideoAd = MobileAds.getRewardedVideoAdInstance(this)
-        rewardedVideoAd.rewardedVideoAdListener = object : RewardedVideoAdListener {
-            override fun onRewardedVideoAdLoaded() {
-                Log.d("SADefaults/AdMob", "Video Ad Loaded")
-            }
-
-            override fun onRewardedVideoAdOpened() {
-                Log.d("SADefaults/AdMob", "Video Ad opened")
-            }
-
-            override fun onRewardedVideoStarted() {
-                Log.d("SADefaults/AdMob", "Video Ad Started")
-            }
-
-            override fun onRewardedVideoAdClosed() {
-                Log.d("SADefaults/AdMob", "Video AD Closed")
-                requestVideoAd()
-            }
-
-            override fun onRewarded(rewardItem: RewardItem) {
-                Log.d("SADefaults/AdMob", "Video Ad Rewarded")
-            }
-
-            override fun onRewardedVideoAdLeftApplication() {
-                Log.d("SADefaults/AdMob", "Video Ad Left app")
-            }
-
-            override fun onRewardedVideoAdFailedToLoad(i: Int) {
-                Log.d("SADefaults/AdMob", "Video Ad Failed to load")
-            }
-
-            override fun onRewardedVideoCompleted() {
-                Log.d("SADefaults/AdMob", "Video Ad Completed")
-            }
-        }
-    }
-
-    private fun configureInterstitialAd() {
-        interstitialAd = InterstitialAd(this)
-        interstitialAd.adUnitId = getString(R.string.admob_interstitial_ad_id)
-        interstitialAd.adListener = object : AdListener() {
-            override fun onAdClosed() {
-                Log.d("SADefaults/AdMob", "Interstitial ad closed")
-                requestInterstitialAd()
-            }
-
-            override fun onAdFailedToLoad(i: Int) {
-                Log.d("SADefaults/AdMob", "Interstitial ad failed to load")
-            }
-
-            override fun onAdLeftApplication() {
-                Log.d("SADefaults/AdMob", "Interstitial ad left application")
-            }
-
-            override fun onAdOpened() {
-                Log.d("SADefaults/AdMob", "Interstitial ad opened")
-            }
-
-            override fun onAdLoaded() {
-                Log.d("SADefaults/AdMob", "Interstitial ad loaded")
-            }
-        }
-    }
-
-    private fun configureBannerAd() {
         adView = findViewById(R.id.adView)
-        adView.adListener = object : AdListener() {
-            override fun onAdClosed() {
-                Log.d("SADefaults/AdMob", "Banner ad closed")
-            }
-
-            override fun onAdFailedToLoad(i: Int) {
-                Log.d("SADefaults/AdMob", "Banner ad failed to load")
-            }
-
-            override fun onAdLeftApplication() {
-                Log.d("SADefaults/AdMob", "Banner ad left application")
-            }
-
-            override fun onAdOpened() {
-                Log.d("SADefaults/AdMob", "Banner ad opened")
-            }
-
-            override fun onAdLoaded() {
-                Log.d("SADefaults/AdMob", "Banner ad loaded")
-            }
-        }
+        requestBannerAd()
     }
 
     private fun requestBannerAd() {
         val bundle = SAAdMobExtras.extras()
-                .setTestMode(false)
-                .setConfiguration(configuration)
-                .setParentalGate(false)
-                .setTransparent(true)
-                .build()
-        adView.loadAd(AdRequest.Builder().addCustomEventExtrasBundle(SAAdMobBannerCustomEvent::class.java, bundle).build())
+            .setTestMode(false)
+            .setParentalGate(false)
+            .setTransparent(true)
+            .build()
+        adView.loadAd(
+            AdRequest.Builder()
+                .addCustomEventExtrasBundle(SAAdMobBannerCustomEvent::class.java, bundle).build()
+        )
     }
 
     private fun requestInterstitialAd() {
         val bundle = SAAdMobExtras.extras()
-                .setTestMode(false)
-                .setConfiguration(configuration)
-                .setOrientation(SAOrientation.PORTRAIT)
-                .setParentalGate(true)
-                .build()
-        interstitialAd.loadAd(AdRequest.Builder().addCustomEventExtrasBundle(SAAdMobInterstitialCustomEvent::class.java, bundle).build())
+            .setTestMode(false)
+            .setOrientation(SAOrientation.PORTRAIT)
+            .setParentalGate(true)
+            .build()
+        AdManagerInterstitialAd.load(
+            this,
+            getString(R.string.admob_interstitial_ad_id),
+            AdManagerAdRequest.Builder().addCustomEventExtrasBundle(
+                SAAdMobInterstitialCustomEvent::class.java, bundle
+            ).build(),
+            object : InterstitialAdLoadCallback() {
+                override fun onAdLoaded(p0: InterstitialAd) {
+                    super.onAdLoaded(p0)
+                    interstitialAd = p0
+                    interstitialAd?.show(this@AdMobActivity)
+                }
+
+                override fun onAdFailedToLoad(p0: LoadAdError) {
+                    super.onAdFailedToLoad(p0)
+                }
+            })
     }
 
     private fun requestVideoAd() {
-        val bundle = SAAdMobExtras.extras()
-                .setTestMode(false)
-                .setConfiguration(configuration)
-                .setParentalGate(false)
-                .setOrientation(SAOrientation.LANDSCAPE)
-                .setSmallClick(true)
-                .setCloseAtEnd(true)
-                .setCloseButton(true)
-                .build()
-        rewardedVideoAd.loadAd(getString(R.string.admob_rewarded_ad_id),
-                AdRequest.Builder().addNetworkExtrasBundle(SAAdMobVideoMediationAdapter::class.java, bundle).build())
-    }
+        val videoBundle = SAAdMobExtras.extras()
+            .setTestMode(false)
+            .setParentalGate(false)
+            .setOrientation(SAOrientation.LANDSCAPE)
+            .setSmallClick(true)
+            .setCloseAtEnd(true)
+            .setCloseButton(true)
+            .build()
+        RewardedAd.load(this,
+            getString(R.string.play_admob_video),
+            AdManagerAdRequest.Builder()
+                .addNetworkExtrasBundle(SAAdMobVideoMediationAdapter::class.java, videoBundle)
+                .build(),
+            object : RewardedAdLoadCallback() {
+                override fun onAdFailedToLoad(error: LoadAdError) {
+                    super.onAdFailedToLoad(error)
+                    Log.e(tag, error.message)
+                }
 
+                override fun onAdLoaded(p0: RewardedAd) {
+                    super.onAdLoaded(p0)
+                    rewardedAd = p0
+                    rewardedAd?.show(this@AdMobActivity) {
+                        Log.e(tag, it.amount.toString())
+                    }
+                }
+            })
+    }
     fun playInterstitial(view: View?) {
-        if (interstitialAd.isLoaded) {
-            interstitialAd.show()
-        } else {
-            Log.d("SADefaults/Admob", "Interstitial not loaded yet")
-        }
+        requestInterstitialAd()
     }
-
     fun playVideo(view: View?) {
-        if (rewardedVideoAd.isLoaded) {
-            rewardedVideoAd.show()
-        } else {
-            Log.d("SADefaults/Admob", "Video Ad not loaded yet")
-        }
+        requestVideoAd()
     }
-
     override fun onDestroy() {
         super.onDestroy()
         adView.destroy()
