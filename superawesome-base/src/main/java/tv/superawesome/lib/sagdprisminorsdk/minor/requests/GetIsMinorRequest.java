b04451f6e42983c2359ab5d7bebe4abea2315a7d
Mark Gilchrist <39841863+markGilchrist@users.noreply.github.com>
2021-06-08 18:47:30 +0100

    update check age (#97)

--- superawesome-base/src/main/java/tv/superawesome/lib/sagdprisminorsdk/minor/requests/GetIsMinorRequest.java
+++ superawesome-base/src/main/java/tv/superawesome/lib/sagdprisminorsdk/minor/requests/GetIsMinorRequest.java
@@ -13,61 +13,62 @@ import tv.superawesome.lib.sajsonparser.SAJsonParser;
 
 public class GetIsMinorRequest extends Service {
 
-    private GetIsMinorInterface listener = null;
-    private String bundleId;
-    private String dateOfBirth;
+  private GetIsMinorInterface listener = null;
+  private String bundleId;
+  private String dateOfBirth;
 
-    public GetIsMinorRequest(String bundleId, String dateOfBirth) {
-        this.bundleId = bundleId;
-        this.dateOfBirth = dateOfBirth;
+  public GetIsMinorRequest(String bundleId, String dateOfBirth) {
+    this.bundleId = bundleId;
+    this.dateOfBirth = dateOfBirth;
+  }
+
+  public GetIsMinorRequest() {
+
+    listener = isMinorModel -> {};
+  }
+
+  @Override
+  public String getEndpoint() {
+    return "v1/countries/child-age";
+  }
+
+  @Override
+  public JSONObject getQuery() {
+    return SAJsonParser.newObject("bundleId", bundleId, "dob", dateOfBirth);
+  }
+
+  @Override
+  public JSONObject getHeader() {
+    return SAJsonParser.newObject();
+  }
+
+  @Override
+  public HTTPMethod getMethod() {
+    return HTTPMethod.GET;
+  }
+
+  @Override
+  public void success(int status, String payload, boolean success) {
+
+    if (!success) {
+      listener.getIsMinorData(null);
+    } else {
+      if (status == 200 && payload != null) {
+
+        GetIsMinorModel isMinorModel = new GetIsMinorModel(payload);
+        listener.getIsMinorData(isMinorModel);
+      } else {
+        listener.getIsMinorData(null);
+      }
     }
+  }
 
-    public GetIsMinorRequest() {
-
-        listener = isMinorModel -> {
-        };
-    }
-
-    @Override
-    public String getEndpoint() {
-        return "v1/countries/child-age";
-    }
-
-    @Override
-    public JSONObject getQuery() {
-        return SAJsonParser.newObject("bundleId", bundleId, "dob", dateOfBirth);
-    }
-
-    @Override
-    public JSONObject getHeader() {
-        return SAJsonParser.newObject();
-    }
-
-    @Override
-    public HTTPMethod getMethod() {
-        return HTTPMethod.GET;
-    }
-
-    @Override
-    public void success(int status, String payload, boolean success) {
-
-        if (!success) {
-            listener.getIsMinorData(null);
-        } else {
-            if (status == 200 && payload != null) {
-
-                GetIsMinorModel isMinorModel = new GetIsMinorModel(payload);
-                listener.getIsMinorData(isMinorModel);
-            } else {
-                listener.getIsMinorData(null);
-            }
-        }
-    }
-
-    public void execute(Context context, String dateOfBirth, String bundleId, ServiceResponseInterface listener) {
-        this.bundleId = bundleId;
-        this.dateOfBirth = dateOfBirth;
-        this.listener = listener != null ? (GetIsMinorInterface) listener : this.listener;
-        super.execute(context, dateOfBirth, bundleId, this.listener);
-    }
+  @Override
+  public void execute(
+      Context context, String dateOfBirth, String bundleId, ServiceResponseInterface listener) {
+    this.bundleId = bundleId;
+    this.dateOfBirth = dateOfBirth;
+    this.listener = listener != null ? (GetIsMinorInterface) listener : this.listener;
+    super.execute(context, dateOfBirth, bundleId, this.listener);
+  }
 }
