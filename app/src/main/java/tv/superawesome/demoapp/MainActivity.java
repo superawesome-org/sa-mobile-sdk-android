a271ae30299959630e255b029702881015368812
Mark Gilchrist <39841863+markGilchrist@users.noreply.github.com>
2021-07-15 11:49:40 +0100

    update Url for line Item (#99)
    * update Url for line Item
    
    * remove duplicate method

--- app/src/main/java/tv/superawesome/demoapp/MainActivity.java
+++ app/src/main/java/tv/superawesome/demoapp/MainActivity.java
@@ -21,184 +21,202 @@ import java.util.List;
 
 import tv.superawesome.lib.sabumperpage.SABumperPage;
 import tv.superawesome.sdk.publisher.AwesomeAds;
+import tv.superawesome.sdk.publisher.SAEvent;
 import tv.superawesome.sdk.publisher.SAInterface;
+import tv.superawesome.sdk.publisher.SAInterstitialAd;
 import tv.superawesome.sdk.publisher.managed.SAManagedBannerAd;
-import tv.superawesome.sdk.publisher.managed.SAManagedInterstitialAd;
 
 public class MainActivity extends Activity {
 
-    /**
-     * the options list
-     */
-    @Override
-    protected void onCreate(final Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        SABumperPage.overrideName("Test app");
+  /** the options list */
+  @Override
+  protected void onCreate(final Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    SABumperPage.overrideName("Test app");
 
-        final SAManagedBannerAd managedBannerAd = findViewById(R.id.preview);
-        managedBannerAd.setListener((SAInterface) (placementId, event) -> managedBannerAd.load(placementId));
+    final SAManagedBannerAd managedBannerAd = findViewById(R.id.preview);
+    managedBannerAd.setListener(
+        (SAInterface) (placementId, event) -> managedBannerAd.load(placementId));
 
-        ListView myList = findViewById(R.id.MyList);
-        final List<AdapterItem> data = Arrays.asList(
-                new HeaderItem("Banners"),
-                new PlacementItem("Banner 300x250 (Tablet MPU)", 61296, Type.BANNER),
-                new PlacementItem("Banner 300x50 (Mobile Small Leaderboard)", 61294, Type.BANNER),
-                new PlacementItem("Banner 320x50 (Mobile Leaderboard)", 61297, Type.BANNER),
-                new HeaderItem("Interstitials"),
-                new PlacementItem("Insterstitial - Rich Media", 61295, Type.INTERSTITIAL),
-                new PlacementItem("Insterstitial - Static", 61298, Type.INTERSTITIAL),
-                new HeaderItem("KSF"),
-                new PlacementItem("Insterstitial - Static", 61321, Type.INTERSTITIAL),
-                new PlacementItem("Insterstitial - Rich Media", 61320, Type.INTERSTITIAL)
-        );
-        ListAdapter<AdapterItem> adapter = new ListAdapter<>(this);
-        myList.setAdapter(adapter);
-        adapter.updateData(data);
-        adapter.reloadList();
+    ListView myList = findViewById(R.id.MyList);
+    final List<AdapterItem> data =
+        Arrays.asList(
+            new HeaderItem("Banners"),
+            new PlacementItem("Banner 300x250 (Tablet MPU)", 61296, Type.BANNER),
+            new PlacementItem("Banner 300x50 (Mobile Small Leaderboard)", 61294, Type.BANNER),
+            new PlacementItem("Banner 320x50 (Mobile Leaderboard)", 61297, Type.BANNER),
+            new HeaderItem("Interstitials"),
+            new PlacementItem("Insterstitial - Rich Media", 61295, Type.INTERSTITIAL),
+            new PlacementItem("Insterstitial - Static", 61298, Type.INTERSTITIAL),
+            new HeaderItem("KSF"),
+            new PlacementItem("Insterstitial - Static", 61321, Type.INTERSTITIAL),
+            new PlacementItem("Insterstitial - Rich Media", 61320, Type.INTERSTITIAL));
+    ListAdapter<AdapterItem> adapter = new ListAdapter<>(this);
+    myList.setAdapter(adapter);
+    adapter.updateData(data);
+    adapter.reloadList();
 
-        myList.setOnItemClickListener((parent, view, position, id) -> {
+    myList.setOnItemClickListener(
+        (parent, view, position, id) -> {
+          AdapterItem item = data.get(position);
+          if (item instanceof PlacementItem) {
+            PlacementItem placement = (PlacementItem) item;
 
-            AdapterItem item = data.get(position);
-            if (item instanceof PlacementItem) {
-                PlacementItem placement = (PlacementItem) item;
-
-                switch (placement.type) {
-                    case BANNER:
-                        managedBannerAd.load(placement.pid);
-                        break;
-                    case INTERSTITIAL:
-                        SAManagedInterstitialAd.load(MainActivity.this, placement.pid);
-                        break;
-                }
+            switch (placement.type) {
+              case BANNER:
+                managedBannerAd.load(placement.pid);
+                break;
+              case INTERSTITIAL:
+                SAInterstitialAd.load(58166, 143306, 437339, MainActivity.this);
+                break;
             }
+          }
         });
-    }
 
-    public void gotoAdMob(View view) {
-        Intent intent = new Intent(this, AdMobActivity.class);
-        this.startActivity(intent);
-    }
+    SAInterstitialAd.setListener((SAInterface) (placementId, event) -> {
+      if (event == SAEvent.adLoaded){
+        SAInterstitialAd.play(placementId, MainActivity.this);
+      }
+    });
+  }
 
-    public void gotoMoPub(View view) {
-        Intent intent = new Intent(this, NewTestActivity.class);
-        this.startActivity(intent);
-    }
+  public void gotoAdMob(View view) {
+    Intent intent = new Intent(this, AdMobActivity.class);
+    this.startActivity(intent);
+  }
 
-    public void ageCheck(View view) {
+  public void gotoMoPub(View view) {
+    Intent intent = new Intent(this, NewTestActivity.class);
+    this.startActivity(intent);
+  }
 
-        final String dateOfBirth = "2012-02-02";
+  public void ageCheck(View view) {
 
-        AwesomeAds.triggerAgeCheck(this, dateOfBirth, isMinorModel -> {
+    final String dateOfBirth = "2012-02-02";
 
-            String message;
-            if (isMinorModel != null) {
-                message = "Min age for '" + isMinorModel.getCountry() + "' is '"
-                        + isMinorModel.getConsentAgeForCountry() + "' "
-                        + "\nThe age is '" + isMinorModel.getAge() + "' "
-                        + "'.\nIs '" + dateOfBirth + "' a minor? -> '" + isMinorModel.isMinor() + "'";
-            } else {
-                message = "Oops! Something went wrong. No valid model for 'Age Check'...";
-            }
+    AwesomeAds.triggerAgeCheck(
+        this,
+        dateOfBirth,
+        isMinorModel -> {
+          String message;
+          if (isMinorModel != null) {
+            message =
+                "Min age for '"
+                    + isMinorModel.getCountry()
+                    + "' is '"
+                    + isMinorModel.getConsentAgeForCountry()
+                    + "' "
+                    + "\nThe age is '"
+                    + isMinorModel.getAge()
+                    + "' "
+                    + "'.\nIs '"
+                    + dateOfBirth
+                    + "' a minor? -> '"
+                    + isMinorModel.isMinor()
+                    + "'";
+          } else {
+            message = "Oops! Something went wrong. No valid model for 'Age Check'...";
+          }
 
-            Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
+          Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
         });
-    }
+  }
 }
 
 enum Type {
-    BANNER,
-    INTERSTITIAL,
-    VIDEO
+  BANNER,
+  INTERSTITIAL,
+  VIDEO
 }
 
 class AdapterItem {
-    // do nothing
+  // do nothing
 }
 
 class HeaderItem extends AdapterItem {
-    public final String title;
+  public final String title;
 
-    public HeaderItem(String title) {
-        this.title = title;
-    }
+  public HeaderItem(String title) {
+    this.title = title;
+  }
 }
 
 class PlacementItem extends AdapterItem {
-    public final String name;
-    public final int pid;
-    public final Type type;
+  public final String name;
+  public final int pid;
+  public final Type type;
 
-    public PlacementItem(String name, int pid, Type type) {
-        this.name = name;
-        this.pid = pid;
-        this.type = type;
-    }
+  public PlacementItem(String name, int pid, Type type) {
+    this.name = name;
+    this.pid = pid;
+    this.type = type;
+  }
 }
 
 class ListAdapter<T extends AdapterItem> extends ArrayAdapter<T> {
 
-    private List<T> data;
+  private List<T> data;
 
-    public ListAdapter(Context context) {
-        super(context, 0);
+  public ListAdapter(Context context) {
+    super(context, 0);
+  }
+
+  public void updateData(List<T> newData) {
+    data = newData;
+  }
+
+  public void reloadList() {
+    notifyDataSetChanged();
+  }
+
+  @Nullable
+  @Override
+  public T getItem(int i) {
+    return data.get(i);
+  }
+
+  @Override
+  public int getViewTypeCount() {
+    return 2;
+  }
+
+  @Override
+  public int getItemViewType(int i) {
+    return getItem(i) instanceof HeaderItem ? 0 : 1;
+  }
+
+  @Override
+  public int getCount() {
+    return data != null ? data.size() : 0;
+  }
+
+  @NonNull
+  @Override
+  public View getView(int position, View convertView, @NonNull ViewGroup parent) {
+
+    if (convertView == null) {
+      convertView =
+          LayoutInflater.from(getContext()).inflate(R.layout.row_placement, parent, false);
     }
 
-    public void updateData(List<T> newData) {
-        data = newData;
+    TextView title = convertView.findViewById(R.id.RowTitle);
+
+    T item = getItem(position);
+
+    if (item instanceof HeaderItem) {
+      HeaderItem header = (HeaderItem) item;
+      title.setText(header.title);
+      convertView.setBackgroundColor(Color.LTGRAY);
+    } else {
+      PlacementItem placement = (PlacementItem) item;
+      if (placement != null) {
+        String titleText = placement.pid + " | " + placement.name;
+        title.setText(titleText);
+      }
+      title.setBackgroundColor(Color.WHITE);
     }
 
-    public void reloadList() {
-        notifyDataSetChanged();
-    }
-
-    @Nullable
-    @Override
-    public T getItem(int i) {
-        return data.get(i);
-    }
-
-    @Override
-    public int getViewTypeCount() {
-        return 2;
-    }
-
-    @Override
-    public int getItemViewType(int i) {
-        return getItem(i) instanceof HeaderItem ? 0 : 1;
-    }
-
-    @Override
-    public int getCount() {
-        return data != null ? data.size() : 0;
-    }
-
-    @NonNull
-    @Override
-    public View getView(int position, View convertView, @NonNull ViewGroup parent) {
-
-        if (convertView == null) {
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.row_placement, parent, false);
-        }
-
-        TextView title = convertView.findViewById(R.id.RowTitle);
-
-        T item = getItem(position);
-
-        if (item instanceof HeaderItem) {
-            HeaderItem header = (HeaderItem) item;
-            title.setText(header.title);
-            convertView.setBackgroundColor(Color.LTGRAY);
-        } else {
-            PlacementItem placement = (PlacementItem) item;
-            if (placement != null) {
-                String titleText = placement.pid + " | " + placement.name;
-                title.setText(titleText);
-            }
-            title.setBackgroundColor(Color.WHITE);
-        }
-
-
-        return convertView;
-    }
+    return convertView;
+  }
 }
