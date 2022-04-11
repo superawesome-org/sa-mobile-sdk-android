c97948c40490dfc08a583277b8a9883edb18b87f
gnhn <cgnhnc@gmail.com>
2022-04-11 17:35:00 +0100

    add: JS bridge to listen events from the WebView (#146)
    * add: JS bridge to listen events from the WebView
    
    * fix: production ksf placement ids
    
    * delete: mopub
    
    * fix: conflicts to add new clock parameter
    
    Co-authored-by: Gunhan Sancar <gunhan.sancar@superawesome.com>

---
+++ superawesome-base/src/main/java/tv/superawesome/sdk/publisher/managed/AdViewJavaScriptBridge.kt
@@ -0,0 +1,52 @@
+package tv.superawesome.sdk.publisher.managed
+
+import android.webkit.JavascriptInterface
+
+class AdViewJavaScriptBridge(private val listener: Listener) {
+    @JavascriptInterface
+    fun adLoaded() = listener.adLoaded()
+
+    @JavascriptInterface
+    fun adEmpty() = listener.adEmpty()
+
+    @JavascriptInterface
+    fun adFailedToLoad() = listener.adFailedToLoad()
+
+    @JavascriptInterface
+    fun adAlreadyLoaded() = listener.adAlreadyLoaded()
+
+    @JavascriptInterface
+    fun adShown() = listener.adShown()
+
+    @JavascriptInterface
+    fun adFailedToShow() = listener.adFailedToShow()
+
+    @JavascriptInterface
+    fun adClicked() = listener.adClicked()
+
+    @JavascriptInterface
+    fun adEnded() = listener.adEnded()
+
+    @JavascriptInterface
+    fun adClosed() = listener.adClosed()
+
+    interface Listener {
+        fun adLoaded()
+
+        fun adEmpty()
+
+        fun adFailedToLoad()
+
+        fun adAlreadyLoaded()
+
+        fun adShown()
+
+        fun adFailedToShow()
+
+        fun adClicked()
+
+        fun adEnded()
+
+        fun adClosed()
+    }
+}
---
+++ superawesome-base/src/main/java/tv/superawesome/sdk/publisher/managed/SAManagedAdActivity.kt
@@ -0,0 +1,121 @@
+package tv.superawesome.sdk.publisher.managed
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.RelativeLayout
+import tv.superawesome.lib.sautils.SAImageUtils
+import tv.superawesome.lib.sautils.SAUtils
+import tv.superawesome.sdk.publisher.SAEvent
+import tv.superawesome.sdk.publisher.SAInterface
+import tv.superawesome.sdk.publisher.SAVideoAd
+
+class SAManagedAdActivity : Activity(), AdViewJavaScriptBridge.Listener {
+    private var listener: SAInterface? = null
+
+    private val placementId by lazy {
+        intent.getIntExtra(PLACEMENT_ID_KEY, 0)
+    }
+
+    private val html by lazy {
+        intent.getStringExtra(HTML_KEY) ?: ""
+    }
+
+    private val adView by lazy {
+        SAManagedAdView(this)
+    }
+
+    private val closeButton by lazy {
+        val size = (SAUtils.getScaleFactor(this) * 30).toInt()
+
+        val buttonLayout = RelativeLayout.LayoutParams(size, size)
+        buttonLayout.addRule(RelativeLayout.ALIGN_PARENT_RIGHT)
+        buttonLayout.addRule(RelativeLayout.ALIGN_PARENT_TOP)
+
+        val closeButton = ImageButton(this)
+        closeButton.visibility = View.VISIBLE
+        closeButton.setImageBitmap(SAImageUtils.createCloseButtonBitmap())
+        closeButton.setBackgroundColor(Color.TRANSPARENT)
+        closeButton.setPadding(0, 0, 0, 0)
+        closeButton.scaleType = ImageView.ScaleType.FIT_XY
+        closeButton.layoutParams = buttonLayout
+        closeButton.setOnClickListener { close() }
+
+        return@lazy closeButton
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(adView)
+        adView.load(placementId, html, this)
+        adView.addView(closeButton)
+
+        listener = SAVideoAd.getListener()
+    }
+
+
+    override fun adLoaded() {
+        listener?.onEvent(this.placementId, SAEvent.adLoaded)
+    }
+
+    override fun adEmpty() {
+        listener?.onEvent(this.placementId, SAEvent.adEmpty)
+        close()
+    }
+
+    override fun adFailedToLoad() {
+        listener?.onEvent(this.placementId, SAEvent.adFailedToLoad)
+        close()
+    }
+
+    override fun adAlreadyLoaded() {
+        listener?.onEvent(this.placementId, SAEvent.adAlreadyLoaded)
+    }
+
+    override fun adShown() {
+        listener?.onEvent(this.placementId, SAEvent.adShown)
+    }
+
+    override fun adFailedToShow() {
+        listener?.onEvent(this.placementId, SAEvent.adFailedToShow)
+        close()
+    }
+
+    override fun adClicked() {
+        listener?.onEvent(this.placementId, SAEvent.adClicked)
+    }
+
+    override fun adEnded() {
+        listener?.onEvent(this.placementId, SAEvent.adEnded)
+        close()
+    }
+
+    override fun adClosed() {
+        listener?.onEvent(this.placementId, SAEvent.adClosed)
+        close()
+    }
+
+    private fun close() {
+        if (!isFinishing) {
+            finish()
+        }
+    }
+
+    companion object {
+        private const val PLACEMENT_ID_KEY = "PLACEMENT_ID"
+        private const val HTML_KEY = "HTML"
+
+        @JvmStatic
+        fun newInstance(context: Context, placementId: Int, html: String): Intent =
+            Intent(context, SAManagedAdActivity::class.java).apply {
+                putExtra(PLACEMENT_ID_KEY, placementId)
+                putExtra(HTML_KEY, html)
+            }
+    }
+}
