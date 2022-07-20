a073051422d9bfe1f948cbad8aba45836591aa94
Gunhan Sancar <cgnhnc@gmail.com>
2022-07-20 13:45:45 +0300

    Feature/aag 2208 add mock api (#177)
    * Vapor / WireMock mock server POC tests
    
    * Removed test config
    
    * Cleanup
    
    * add(): wiremock integration and update video ui tests to use that
    
    Co-authored-by: Tom <tom.orourke@superawesome.com>
    Co-authored-by: Gunhan Sancar <gunhan.sancar@superawesome.com>

--- app/src/main/java/tv/superawesome/demoapp/MainActivity.kt
+++ app/src/main/java/tv/superawesome/demoapp/MainActivity.kt
@@ -32,7 +32,6 @@ val data = listOf(
 
 class MainActivity : Activity() {
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
