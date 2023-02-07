426bcdab757c8baf67310d54f2bb36ca8f7a27e3
Gunhan Sancar <cgnhnc@gmail.com>
2023-02-07 14:08:46 +0000

    chore(): fix a bug where bumper page screen doesn't prevent the clicks go through. Also converts bumper page to a dialog instead of an activity. (#259)

---
+++ superawesome-common/src/main/java/tv/superawesome/sdk/publisher/common/ui/common/BumperPage.kt
@@ -0,0 +1,60 @@
+package tv.superawesome.sdk.publisher.common.ui.common
+
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.os.Handler
+import android.os.Looper
+import tv.superawesome.sdk.publisher.common.models.Constants
+import tv.superawesome.sdk.publisher.common.models.VoidBlock
+
+class BumperPage {
+    private var dialog: BumperPageDialog? = null
+    private var handler = Handler(Looper.getMainLooper())
+    private var runnable: Runnable? = null
+    var onFinish: VoidBlock? = null
+
+    fun show(context: Context) {
+        dialog?.dismiss()
+        dialog = BumperPageDialog(context)
+        dialog?.show()
+        setupTimer()
+    }
+
+    fun stop() {
+        dialog?.dismiss()
+        dialog = null
+        runnable?.let { handler.removeCallbacks(it) }
+        runnable = null
+    }
+
+    private fun setupTimer() {
+        val countdown = intArrayOf(Constants.defaultBumperPageShowTimeInSec)
+        handler = Handler(Looper.getMainLooper())
+        runnable = Runnable {
+            if (countdown[0] <= 0) {
+                onFinish?.invoke()
+                stop()
+            } else {
+                countdown[0]--
+                dialog?.updateTimeLeft(countdown[0])
+                runnable?.let { handler.postDelayed(it, 1000) }
+            }
+        }
+        runnable?.let { handler.postDelayed(it, 1000) }
+    }
+
+    companion object {
+        var appName: String? = null
+            private set
+        var appIcon: Drawable? = null
+            private set
+
+        fun overrideName(name: String?) {
+            appName = name
+        }
+
+        fun overrideLogo(drawable: Drawable?) {
+            appIcon = drawable
+        }
+    }
+}
