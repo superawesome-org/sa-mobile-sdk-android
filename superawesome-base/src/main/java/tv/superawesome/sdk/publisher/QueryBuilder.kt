c7dc3617af156727c1706437131df3678f87113e
Tom O'Rourke <thomasjamesorourke@hotmail.co.uk>
2023-02-08 20:35:35 +0000

    chore(): code comments

--- superawesome-base/src/main/java/tv/superawesome/sdk/publisher/QueryBuilder.kt
+++ superawesome-base/src/main/java/tv/superawesome/sdk/publisher/QueryBuilder.kt
@@ -4,6 +4,11 @@ import org.json.JSONObject
 
 class QueryBuilder {
 
+    /**
+     * @param new a dictionary to be merged with an existing dictionary where only String or Int
+     * values are supported.
+     * @param original: the original dictionary to merge with.
+     */
     fun merge(new: Map<String, Any>, original: MutableMap<String, Any>) {
         new.forEach { (key, value) ->
             when (value){
@@ -13,6 +18,11 @@ class QueryBuilder {
         }
     }
 
+    /**
+     * @param new a dictionary to be merged with an existing JSONObject where only String or Int
+     * values are supported.
+     * @param original: the original JSONObject to merge with.
+     */
     fun merge(new: Map<String, Any>, original: JSONObject) {
         new.forEach { (key, value) ->
             when (value){
