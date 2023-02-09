a854bf0ceb946d6dbcf3c64839fa2efa67b639c4
Tom O'Rourke <30452349+thomas-james-1986@users.noreply.github.com>
2023-02-09 18:34:31 +0000

    Update superawesome-base/src/main/java/tv/superawesome/sdk/publisher/QueryBuilder.kt
    Co-authored-by: Myles Eynon <me2k@users.noreply.github.com>

--- superawesome-base/src/main/java/tv/superawesome/sdk/publisher/QueryBuilder.kt
+++ superawesome-base/src/main/java/tv/superawesome/sdk/publisher/QueryBuilder.kt
@@ -11,7 +11,7 @@ class QueryBuilder {
      */
     fun merge(new: Map<String, Any>?, original: MutableMap<String, Any>) {
         new?.forEach { (key, value) ->
-            when (value){
+            when (value) {
                 is String -> original[key] = value
                 is Int -> original[key] = value
             }
