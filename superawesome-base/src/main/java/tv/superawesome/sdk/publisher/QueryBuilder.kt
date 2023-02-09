0605e5c91b24f3dc9ef6c77db3577b449113bff0
Tom O'Rourke <30452349+thomas-james-1986@users.noreply.github.com>
2023-02-09 18:34:49 +0000

    Update superawesome-base/src/main/java/tv/superawesome/sdk/publisher/QueryBuilder.kt
    Co-authored-by: Myles Eynon <me2k@users.noreply.github.com>

--- superawesome-base/src/main/java/tv/superawesome/sdk/publisher/QueryBuilder.kt
+++ superawesome-base/src/main/java/tv/superawesome/sdk/publisher/QueryBuilder.kt
@@ -25,7 +25,7 @@ class QueryBuilder {
      */
     fun merge(new: Map<String, Any>?, original: JSONObject) {
         new?.forEach { (key, value) ->
-            when (value){
+            when (value) {
                 is String -> original.put(key, value)
                 is Int -> original.put(key, value)
             }
