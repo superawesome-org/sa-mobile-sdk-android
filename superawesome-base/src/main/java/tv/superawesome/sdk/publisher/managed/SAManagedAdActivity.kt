1f4835f59d17810e3386594c76a264b2550c59a0
Myles Eynon <me2k@users.noreply.github.com>
2023-02-28 14:52:51 +0000

    AAG-2879: Added timeouts to show and hide the close button on Managed Ads (#283)
    * fix(CloseButton): AAG-2879: Added timeouts to show and hide the close button on Managed Ads
    
    * Minor clean up
    
    * Added missing version properties file, fixed extra adShown event and close for Manage ads
    
    * renamed version file to avoid conflicts

--- superawesome-base/src/main/java/tv/superawesome/sdk/publisher/managed/SAManagedAdActivity.kt
+++ superawesome-base/src/main/java/tv/superawesome/sdk/publisher/managed/SAManagedAdActivity.kt
@@ -5,6 +5,8 @@ import android.content.Context
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.View
 import android.view.WindowManager
 import android.widget.ImageButton
@@ -15,9 +17,16 @@ import tv.superawesome.lib.sautils.SAUtils
 import tv.superawesome.sdk.publisher.SAEvent
 import tv.superawesome.sdk.publisher.SAInterface
 import tv.superawesome.sdk.publisher.SAVideoAd
+import tv.superawesome.sdk.publisher.state.CloseButtonState
+import java.lang.ref.WeakReference
 
 class SAManagedAdActivity : Activity(), AdViewJavaScriptBridge.Listener {
     private var listener: SAInterface? = null
+    private var config: ManangedAdConfig? = null
+    private var timeOutRunnable: Runnable? = null
+    private var shownRunnable: Runnable? = null
+    private var timeOutHandler = Handler(Looper.getMainLooper())
+    private var shownHandler = Handler(Looper.getMainLooper())
 
     private val placementId by lazy {
         intent.getIntExtra(PLACEMENT_ID_KEY, 0)
@@ -39,7 +48,9 @@ class SAManagedAdActivity : Activity(), AdViewJavaScriptBridge.Listener {
         buttonLayout.addRule(RelativeLayout.ALIGN_PARENT_TOP)
 
         val closeButton = ImageButton(this)
-        closeButton.visibility = View.VISIBLE
+        closeButton.visibility =
+            if (config?.closeButtonState == CloseButtonState.VisibleImmediately)
+                View.VISIBLE else View.GONE
         closeButton.setImageBitmap(SAImageUtils.createCloseButtonBitmap())
         closeButton.setBackgroundColor(Color.TRANSPARENT)
         closeButton.setPadding(0, 0, 0, 0)
@@ -55,14 +66,57 @@ class SAManagedAdActivity : Activity(), AdViewJavaScriptBridge.Listener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // get values from the intent
+        config = intent.getParcelableExtra(CONFIG_KEY)
+
         window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
         setContentView(adView)
         adView.contentDescription = "Ad content"
         adView.load(placementId, html, this)
-        adView.addView(closeButton)
-
         listener = SAVideoAd.getListener()
+
+        adView.addView(closeButton)
+        setUpCloseButtonTimeoutRunnable()
     }
+
+    override fun onStop() {
+        super.onStop()
+        cancelCloseButtonTimeoutRunnable()
+        cancelCloseButtonShownRunnable()
+    }
+
+    private fun setUpCloseButtonTimeoutRunnable() {
+        cancelCloseButtonTimeoutRunnable()
+        val weak = WeakReference(this)
+        timeOutRunnable = Runnable {
+            val weakThis = weak.get() ?: return@Runnable
+            weakThis.hasBeenVisibleForRequiredTimeoutTime()
+        }
+        timeOutRunnable?.let { timeOutHandler.postDelayed(it, CLOSE_BUTTON_TIMEOUT_TIME_INTERVAL) }
+    }
+
+    private fun setUpCloseButtonShownRunnable() {
+        cancelCloseButtonShownRunnable()
+        val weak = WeakReference(this)
+        shownRunnable = Runnable {
+            val weakThis = weak.get() ?: return@Runnable
+            weakThis.hasBeenVisibleForRequiredTime()
+        }
+        shownRunnable?.let { shownHandler.postDelayed(it, CLOSE_BUTTON_SHOWN_TIME_INTERVAL) }
+    }
+
+    private fun cancelCloseButtonTimeoutRunnable() {
+        timeOutRunnable?.let { timeOutHandler.removeCallbacks(it) }
+        timeOutRunnable = null
+    }
+
+    private fun cancelCloseButtonShownRunnable() {
+        timeOutRunnable?.let { shownHandler.removeCallbacks(it) }
+        timeOutRunnable = null
+    }
+
+    // AdViewJavaScriptBridge.Listener
 
     override fun adLoaded() {
         listener?.onEvent(this.placementId, SAEvent.adLoaded)
@@ -83,6 +137,10 @@ class SAManagedAdActivity : Activity(), AdViewJavaScriptBridge.Listener {
     }
 
     override fun adShown() {
+        cancelCloseButtonTimeoutRunnable()
+        if (config?.closeButtonState == CloseButtonState.VisibleWithDelay) {
+            setUpCloseButtonShownRunnable()
+        }
         listener?.onEvent(this.placementId, SAEvent.adShown)
     }
 
@@ -100,9 +158,7 @@ class SAManagedAdActivity : Activity(), AdViewJavaScriptBridge.Listener {
         close()
     }
 
-    override fun adClosed() {
-        close()
-    }
+    override fun adClosed() = close()
 
     private fun close() {
         if (!isFinishing) {
@@ -111,9 +167,34 @@ class SAManagedAdActivity : Activity(), AdViewJavaScriptBridge.Listener {
         }
     }
 
+    private fun hasBeenVisibleForRequiredTime() {
+        closeButton.visibility =
+            if (config?.closeButtonState?.isVisible() == true) View.VISIBLE else closeButton.visibility
+    }
+
+    private fun hasBeenVisibleForRequiredTimeoutTime() {
+        closeButton.visibility = View.VISIBLE
+    }
+
+    /**
+     * Overridden "onBackPressed" method of the activity
+     * Depending on how the ad is customised, this will lock the back button or it will allow it.
+     * If it allows it, it's going to also send an "adClosed" event back to the SDK user
+     */
+    @Deprecated("Deprecated in Java")
+    override fun onBackPressed() {
+        if (config?.isBackButtonEnabled == true) {
+            close()
+        }
+    }
+
     companion object {
         private const val PLACEMENT_ID_KEY = "PLACEMENT_ID"
         private const val HTML_KEY = "HTML"
+        const val CONFIG_KEY = "CONFIG"
+
+        private const val CLOSE_BUTTON_SHOWN_TIME_INTERVAL = 2000L
+        private const val CLOSE_BUTTON_TIMEOUT_TIME_INTERVAL = 12000L
 
         @JvmStatic
         fun newInstance(context: Context, placementId: Int, html: String): Intent =
