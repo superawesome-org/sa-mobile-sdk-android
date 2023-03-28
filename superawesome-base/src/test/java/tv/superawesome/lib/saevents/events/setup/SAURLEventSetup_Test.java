b906326ab91d2d1a0d50812b095df2644e6dced4
Myles Eynon <myles.eynon@superawesome.com>
2023-03-28 18:07:40 +0100

    fix(Jacoco): Added JacocoTestReport and renamed test files

---
+++ superawesome-base/src/test/java/tv/superawesome/lib/saevents/events/setup/SAURLEventSetup_Test.java
@@ -0,0 +1,30 @@
+package tv.superawesome.lib.saevents.events.setup;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import tv.superawesome.lib.saevents.events.SAURLEvent;
+import tv.superawesome.lib.saevents.events.Event_Test;
+
+/**
+ * Created by gabriel.coman on 09/05/2018.
+ */
+public class SAURLEventSetup_Test extends Event_Test {
+
+    @Test
+    public void test_SAURLEvent_Init () {
+        // given
+        String url = "https://api.url/abc";
+
+        // when
+        SAURLEvent event = new SAURLEvent(url, super.executor, 1000, true);
+
+        // then - endpoint
+        Assert.assertNotNull(event.getEndpoint());
+        Assert.assertEquals("", event.getEndpoint());
+
+        // then - url
+        Assert.assertNotNull(event.getUrl());
+        Assert.assertEquals("https://api.url/abc", event.getUrl());
+    }
+}
