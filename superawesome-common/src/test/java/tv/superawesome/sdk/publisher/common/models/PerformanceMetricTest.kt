278825b294b853aef0698f5198798f0f752fc07d
Tom O'Rourke <thomasjamesorourke@hotmail.co.uk>
2023-05-31 22:02:36 +0100

    chore(): cleanup

--- superawesome-common/src/test/java/tv/superawesome/sdk/publisher/common/models/PerformanceMetricTest.kt
+++ superawesome-common/src/test/java/tv/superawesome/sdk/publisher/common/models/PerformanceMetricTest.kt
@@ -6,54 +6,54 @@ import kotlin.test.assertEquals
 
 internal class PerformanceMetricTest : BaseTest() {
 
-  @Test
-  fun test_metricName() {
-      val closeButtonPressTime = PerformanceMetricName.CloseButtonPressTime
-      val dwellTime = PerformanceMetricName.DwellTime
-      val loadTime = PerformanceMetricName.LoadTime
-      val renderTime = PerformanceMetricName.RenderTime
+    @Test
+    fun test_metricName() {
+        val closeButtonPressTime = PerformanceMetricName.CloseButtonPressTime
+        val dwellTime = PerformanceMetricName.DwellTime
+        val loadTime = PerformanceMetricName.LoadTime
+        val renderTime = PerformanceMetricName.RenderTime
 
-      assertEquals("sa.ad.sdk.close.button.press.time", closeButtonPressTime.value)
-      assertEquals("sa.ad.sdk.dwell.time", dwellTime.value)
-      assertEquals("sa.ad.sdk.performance.load.time", loadTime.value)
-      assertEquals("sa.ad.sdk.performance.render.time", renderTime.value)
-  }
+        assertEquals("sa.ad.sdk.close.button.press.time", closeButtonPressTime.value)
+        assertEquals("sa.ad.sdk.dwell.time", dwellTime.value)
+        assertEquals("sa.ad.sdk.performance.load.time", loadTime.value)
+        assertEquals("sa.ad.sdk.performance.render.time", renderTime.value)
+    }
 
-  @Test
-  fun test_metricType() {
-      val gauge = PerformanceMetricType.Gauge
-      val increment = PerformanceMetricType.Increment
-      val decrementBy = PerformanceMetricType.DecrementBy
-      val decrement = PerformanceMetricType.Decrement
-      val histogram = PerformanceMetricType.Histogram
-      val incrementBy = PerformanceMetricType.IncrementBy
-      val timing = PerformanceMetricType.Timing
+    @Test
+    fun test_metricType() {
+        val gauge = PerformanceMetricType.Gauge
+        val increment = PerformanceMetricType.Increment
+        val decrementBy = PerformanceMetricType.DecrementBy
+        val decrement = PerformanceMetricType.Decrement
+        val histogram = PerformanceMetricType.Histogram
+        val incrementBy = PerformanceMetricType.IncrementBy
+        val timing = PerformanceMetricType.Timing
 
-      assertEquals("gauge", gauge.value)
-      assertEquals("increment", increment.value)
-      assertEquals("decrementBy", decrementBy.value)
-      assertEquals("decrement", decrement.value)
-      assertEquals("histogram", histogram.value)
-      assertEquals("incrementBy", incrementBy.value)
-      assertEquals("timing", timing.value)
-  }
+        assertEquals("gauge", gauge.value)
+        assertEquals("increment", increment.value)
+        assertEquals("decrementBy", decrementBy.value)
+        assertEquals("decrement", decrement.value)
+        assertEquals("histogram", histogram.value)
+        assertEquals("incrementBy", incrementBy.value)
+        assertEquals("timing", timing.value)
+    }
 
-  @Test
-  fun test_Build() {
-      // Given
-      val value = 10.0
-      val metricName = PerformanceMetricName.LoadTime
-      val metricType = PerformanceMetricType.Gauge
+    @Test
+    fun test_Build() {
+        // Given
+        val value = 10.0
+        val metricName = PerformanceMetricName.LoadTime
+        val metricType = PerformanceMetricType.Gauge
 
-      // When
-      val metric = PerformanceMetric(value, metricName, metricType)
-      val expectedMap = mapOf(
-          "value" to 10.0,
-          "metricName" to "sa.ad.sdk.performance.load.time",
-          "metricType" to "gauge"
-      )
+        // When
+        val metric = PerformanceMetric(value, metricName, metricType)
+        val expectedMap = mapOf(
+            "value" to 10.0,
+            "metricName" to "sa.ad.sdk.performance.load.time",
+            "metricType" to "gauge"
+        )
 
-      // Then
-      assertEquals(expectedMap, metric.build())
-  }
+        // Then
+        assertEquals(expectedMap, metric.build())
+    }
 }
