cb5b9dfd92a3bebeca67736a4172cc835a5c24e6
Myles Eynon <me2k@users.noreply.github.com>
2023-08-02 10:32:37 +0100

    chore(Gradle): removed location and storage permissions and added targetSdk version of 33 (#376)

--- gradle/build-logic/src/main/kotlin/ads.sdk.android.app.gradle.kts
+++ gradle/build-logic/src/main/kotlin/ads.sdk.android.app.gradle.kts
@@ -14,6 +14,7 @@ android {
     configureKotlinAndroid(this)
 
     defaultConfig {
+        targetSdk = Versions.targetSdk
         versionCode = Versions.versionCode
         versionName = Versions.versionName
 
