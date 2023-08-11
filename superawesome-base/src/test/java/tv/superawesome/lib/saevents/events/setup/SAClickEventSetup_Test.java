10983e8cfdabfbd33482ce4e3376975d9cde7226
Myles Eynon <me2k@users.noreply.github.com>
2023-08-11 15:02:29 +0100

    chore(AdRequest): Reinstate the rnd  query parameter on the initial ad request (#380)
    * chore(AdRequest): Reverted the missing `rnd` parameter in the ad request in Base
    
    * Added linter suppress message
    
    * Reverted Common Module

--- superawesome-base/src/test/java/tv/superawesome/lib/saevents/events/setup/SAClickEventSetup_Test.java
+++ superawesome-base/src/test/java/tv/superawesome/lib/saevents/events/setup/SAClickEventSetup_Test.java
@@ -36,7 +36,7 @@ public class SAClickEventSetup_Test extends Event_Test {
 
         Assert.assertEquals(2, query.get("ct"));
         Assert.assertEquals(2001, query.get("line_item"));
-        //Assert.assertEquals(123456, query.get("rnd"));
+        Assert.assertEquals("rand", query.get("rnd"));
         Assert.assertEquals("1.0.0", query.get("sdkVersion"));
         Assert.assertEquals(1000, query.get("placement"));
         Assert.assertEquals("superawesome.tv.saadloaderdemo", query.get("bundle"));
@@ -62,7 +62,7 @@ public class SAClickEventSetup_Test extends Event_Test {
 
         Assert.assertEquals(2, query.get("ct"));
         Assert.assertEquals(2001, query.get("line_item"));
-        //Assert.assertEquals(123456, query.get("rnd"));
+        Assert.assertEquals("rand", query.get("rnd"));
         Assert.assertEquals("1.0.0", query.get("sdkVersion"));
         Assert.assertEquals(1000, query.get("placement"));
         Assert.assertEquals("superawesome.tv.saadloaderdemo", query.get("bundle"));
