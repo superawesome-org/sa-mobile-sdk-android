eba14f9fd49d61dae38e75179608d51eb77c51e6
Myles Eynon <myles.eynon@superawesome.com>
2023-08-25 10:00:28 +0100

    Cleaned up logging statements

--- superawesome-common/src/main/java/tv/superawesome/sdk/publisher/common/ui/banner/CustomWebView.kt
+++ superawesome-common/src/main/java/tv/superawesome/sdk/publisher/common/ui/banner/CustomWebView.kt
@@ -135,7 +135,6 @@ internal class CustomWebView @JvmOverloads constructor(
                     "div { margin: 0px; padding: 0px; } html, body { width: 100%; height: 100%; }" +
                     "</style></head><body>$html</body></html>"
         val injectedHtml = omJSInjector.injectJS(baseHtml)
-        Log.d("Mylo", "Loaded JS: $injectedHtml")
         loadDataWithBaseURL(
             base,
             injectedHtml,
