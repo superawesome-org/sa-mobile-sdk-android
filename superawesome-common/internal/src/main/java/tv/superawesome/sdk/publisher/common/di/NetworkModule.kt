23e43ab6f8026855eb57ec2c7258227dbc1c4391
Matheus Fernando Finatti <matheus.finatti@superawesome.com>
2023-08-26 20:47:54 +0100

    chore(common): split networking into its own module

---
+++ superawesome-common/internal/src/main/java/tv/superawesome/sdk/publisher/common/di/NetworkModule.kt
@@ -0,0 +1,65 @@
+package tv.superawesome.sdk.publisher.common.di
+
+import com.chuckerteam.chucker.api.ChuckerCollector
+import com.chuckerteam.chucker.api.ChuckerInterceptor
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.android.ext.koin.androidContext
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import tv.superawesome.sdk.publisher.common.network.AwesomeAdsApi
+import tv.superawesome.sdk.publisher.common.network.datasources.NetworkDataSourceType
+import tv.superawesome.sdk.publisher.common.network.datasources.OkHttpNetworkDataSource
+import tv.superawesome.sdk.publisher.common.network.interceptors.HeaderInterceptor
+import tv.superawesome.sdk.publisher.common.network.interceptors.RetryInterceptor
+
+@OptIn(ExperimentalSerializationApi::class)
+fun networkModule(baseUrl: String) = module {
+
+    single { HeaderInterceptor(get()) }
+    single { RetryInterceptor(maxRetries = 5, get()) }
+    single {
+        val retryInterceptor: RetryInterceptor = get()
+        val headerInterceptor: HeaderInterceptor = get()
+        val httpLoggingInterceptor = HttpLoggingInterceptor()
+        httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+
+        OkHttpClient().newBuilder()
+            .addNetworkInterceptor(retryInterceptor)
+            .addInterceptor(headerInterceptor)
+            .addInterceptor(
+                ChuckerInterceptor.Builder(androidContext())
+                    .collector(ChuckerCollector(androidContext()))
+                    .maxContentLength(length = 250_000L)
+                    .redactHeaders(emptySet())
+                    .alwaysReadResponseBody(false)
+                    .build()
+            )
+            .addInterceptor(httpLoggingInterceptor)
+            .build()
+    }
+
+    single {
+        Json {
+            allowStructuredMapKeys = true
+            ignoreUnknownKeys = true
+        }
+    }
+    single {
+        val json: Json = get()
+        Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .client(get())
+            .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+            .build()
+    }
+    single {
+        val retrofit: Retrofit = get()
+        retrofit.create(AwesomeAdsApi::class.java)
+    }
+    single<NetworkDataSourceType> { OkHttpNetworkDataSource(get(), get(), get()) }
+}
