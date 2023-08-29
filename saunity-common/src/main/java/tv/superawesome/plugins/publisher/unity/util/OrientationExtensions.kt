574584d96d17cc6fb47038a2d22451c0b30fd372
Matheus Fernando Finatti <matheus.finatti@superawesome.com>
2023-08-29 08:46:51 +0100

    fix some detekt issues and code cleanup
    - change getOrientationFromValue visibility to internal
    - suppress RedundantVisibilityModifier from common module
    - Removed explicit package
    - Removed commented code

--- saunity-common/src/main/java/tv/superawesome/plugins/publisher/unity/util/OrientationExtensions.kt
+++ saunity-common/src/main/java/tv/superawesome/plugins/publisher/unity/util/OrientationExtensions.kt
@@ -2,4 +2,4 @@ package tv.superawesome.plugins.publisher.unity.util
 
 import tv.superawesome.sdk.publisher.common.models.Orientation
 
-fun getOrientationFromValue(value: Int): Orientation? = Orientation.fromValue(value)
+internal fun getOrientationFromValue(value: Int): Orientation? = Orientation.fromValue(value)
