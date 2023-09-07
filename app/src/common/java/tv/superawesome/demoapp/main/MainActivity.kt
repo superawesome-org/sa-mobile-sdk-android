1ed9ab307ec3eb698ad279144c7cd27625fd16fa
Matheus Fernando Finatti <matheus.finatti@superawesome.com>
2023-09-07 10:46:33 +0100

    chore(demo): create base and common flavors
    - Common flavor uses the newer common sdk
    - Base flavor uses the legacy sdk

--- app/src/common/java/tv/superawesome/demoapp/main/MainActivity.kt
+++ app/src/common/java/tv/superawesome/demoapp/main/MainActivity.kt
@@ -5,20 +5,18 @@ import android.util.Log
 import android.view.View
 import androidx.activity.viewModels
 import androidx.fragment.app.FragmentActivity
-import tv.superawesome.demoapp.MyApplication
 import tv.superawesome.demoapp.databinding.ActivityMainBinding
 import tv.superawesome.demoapp.model.FeatureType
 import tv.superawesome.demoapp.model.PlacementItem
 import tv.superawesome.demoapp.settings.DataStore
 import tv.superawesome.demoapp.settings.SettingsDialogFragment
-import tv.superawesome.lib.sasession.defines.SAConfiguration
 import tv.superawesome.sdk.publisher.common.models.CloseButtonState
 import tv.superawesome.sdk.publisher.common.models.SAEvent
-import tv.superawesome.sdk.publisher.common.network.Environment
 import tv.superawesome.sdk.publisher.common.sdk.AwesomeAds
 import tv.superawesome.sdk.publisher.common.ui.interstitial.SAInterstitialAd
 import tv.superawesome.sdk.publisher.common.ui.video.SAVideoAd
 
+@Suppress("TooManyFunctions")
 class MainActivity : FragmentActivity() {
     private lateinit var adapter: CustomListAdapter
     private val viewModel: MainViewModel by viewModels()
@@ -81,7 +79,7 @@ class MainActivity : FragmentActivity() {
             Log.i(TAG, "SAInterstitialAd event ${event.name} thread:${Thread.currentThread()}")
 
             if (event == SAEvent.adLoaded && isPlayEnabled()) {
-                SAInterstitialAd.play(placementId, this@MainActivity)
+                SAInterstitialAd.play(placementId, this)
             }
         }
 
@@ -90,11 +88,9 @@ class MainActivity : FragmentActivity() {
             Log.i(TAG, "SAVideoAd event ${event.name} thread:${Thread.currentThread()}")
 
             if (event == SAEvent.adLoaded && isPlayEnabled()) {
-                SAVideoAd.play(placementId, this@MainActivity)
+                SAVideoAd.play(placementId, this)
             }
         }
-
-        setListenersForBase()
     }
 
     private fun setupListView() {
@@ -112,10 +108,6 @@ class MainActivity : FragmentActivity() {
     }
 
     private fun onBannerClick(item: PlacementItem) {
-        if (DataStore.data.useBaseModule) {
-            onBannerClickForBase(item)
-            return
-        }
         if (item.isFull()) {
             binding.bannerView.load(
                 item.placementId,
@@ -128,88 +120,34 @@ class MainActivity : FragmentActivity() {
     }
 
     private fun onInterstitialClick(item: PlacementItem) {
-        if (DataStore.data.useBaseModule) {
-            onInterstitialClickForBase(item)
-            return
-        }
         if (item.isFull()) {
             SAInterstitialAd.load(
                 item.placementId,
                 item.lineItemId ?: 0,
                 item.creativeId ?: 0,
-                this@MainActivity
+                this
             )
         } else {
-            SAInterstitialAd.load(item.placementId, this@MainActivity)
+            SAInterstitialAd.load(item.placementId, this)
         }
     }
 
     private fun onVideoClick(item: PlacementItem) {
-        if (DataStore.data.useBaseModule) {
-            onVideoClickForBase(item)
-            return
-        }
         if (item.isFull()) {
             SAVideoAd.load(
                 item.placementId,
                 item.lineItemId ?: 0,
                 item.creativeId ?: 0,
-                this@MainActivity
+                this
             )
         } else {
-            SAVideoAd.load(item.placementId, this@MainActivity)
-        }
-    }
-
-    private fun onBannerClickForBase(item: PlacementItem) {
-        if (item.isFull()) {
-            binding.bannerView2.load(
-                item.placementId,
-                item.lineItemId ?: 0,
-                item.creativeId ?: 0
-            )
-        } else {
-            binding.bannerView2.load(item.placementId)
-        }
-    }
-
-    private fun onInterstitialClickForBase(item: PlacementItem) {
-        if (item.isFull()) {
-            tv.superawesome.sdk.publisher.SAInterstitialAd.load(
-                item.placementId,
-                item.lineItemId ?: 0,
-                item.creativeId ?: 0,
-                this@MainActivity
-            )
-        } else {
-            tv.superawesome.sdk.publisher.SAInterstitialAd.load(item.placementId, this@MainActivity)
-        }
-    }
-
-    private fun onVideoClickForBase(item: PlacementItem) {
-        if (item.isFull()) {
-            tv.superawesome.sdk.publisher.SAVideoAd.load(
-                item.placementId,
-                item.lineItemId ?: 0,
-                item.creativeId ?: 0,
-                this@MainActivity
-            )
-        } else {
-            tv.superawesome.sdk.publisher.SAVideoAd.load(item.placementId, this@MainActivity)
+            SAVideoAd.load(item.placementId, this)
         }
     }
 
     private fun updateSettings() {
-        val settings = DataStore.data
-
         binding.bannerView.visibility = View.GONE
-        binding.bannerView2.visibility = View.GONE
-
-        if (settings.useBaseModule) {
-            updateSettingsForBase()
-        } else {
-            updateSettingsForCommon()
-        }
+        updateSettingsForCommon()
     }
 
     private fun updateSettingsForCommon() {
@@ -245,81 +183,8 @@ class MainActivity : FragmentActivity() {
         }
     }
 
-    private fun setListenersForBase() {
-        tv.superawesome.sdk.publisher.SAVideoAd.setListener { placementId, event ->
-            updateMessage(placementId, event)
-            Log.i(TAG, "SAVideoAd event ${event.name} thread:${Thread.currentThread()}")
-
-            if (event == tv.superawesome.sdk.publisher.SAEvent.adLoaded && isPlayEnabled()) {
-                tv.superawesome.sdk.publisher.SAVideoAd.play(placementId, this@MainActivity)
-            }
-        }
-
-        tv.superawesome.sdk.publisher.SAInterstitialAd.setListener { placementId, event ->
-            updateMessage(placementId, event)
-            Log.i(TAG, "SAInterstitialAd event ${event.name} thread:${Thread.currentThread()}")
-
-            if (event == tv.superawesome.sdk.publisher.SAEvent.adLoaded && isPlayEnabled()) {
-                tv.superawesome.sdk.publisher.SAInterstitialAd.play(placementId, this@MainActivity)
-            }
-        }
-
-        binding.bannerView2.setListener { placementId, event ->
-            updateMessage(placementId, event)
-            Log.i(TAG, "bannerView2 event ${event.name} thread:${Thread.currentThread()}")
-
-            if (event == tv.superawesome.sdk.publisher.SAEvent.adLoaded && isPlayEnabled()) {
-                binding.bannerView2.play(this)
-            }
-        }
-    }
-
-    private fun updateSettingsForBase() {
-        Log.i(TAG, "updateSettingsForBase")
-
-        val settings = DataStore.data
-        val environment = if ((application as MyApplication).environment == Environment.UITesting)
-            SAConfiguration.UITESTING else SAConfiguration.PRODUCTION
-
-        binding.bannerView2.visibility = View.VISIBLE
-        binding.bannerView2.setBumperPage(settings.bumperEnabled)
-        binding.bannerView2.setParentalGate(settings.parentalEnabled)
-        binding.bannerView2.setConfiguration(environment)
-
-        tv.superawesome.sdk.publisher.SAInterstitialAd.setBumperPage(settings.bumperEnabled)
-        tv.superawesome.sdk.publisher.SAInterstitialAd.setParentalGate(settings.parentalEnabled)
-        tv.superawesome.sdk.publisher.SAInterstitialAd.setConfiguration(environment)
-
-        tv.superawesome.sdk.publisher.SAVideoAd.setBumperPage(settings.bumperEnabled)
-        tv.superawesome.sdk.publisher.SAVideoAd.setParentalGate(settings.parentalEnabled)
-        tv.superawesome.sdk.publisher.SAVideoAd.setMuteOnStart(settings.muteOnStart)
-        tv.superawesome.sdk.publisher.SAVideoAd.setCloseButtonWarning(settings.videoWarnOnClose)
-        tv.superawesome.sdk.publisher.SAVideoAd.setCloseAtEnd(settings.closeAtEnd)
-        tv.superawesome.sdk.publisher.SAVideoAd.setConfiguration(environment)
-
-        when (settings.closeButtonState) {
-            CloseButtonState.VisibleImmediately -> {
-                tv.superawesome.sdk.publisher.SAVideoAd.enableCloseButtonNoDelay()
-                tv.superawesome.sdk.publisher.SAInterstitialAd.enableCloseButtonNoDelay()
-            }
-
-            CloseButtonState.VisibleWithDelay -> {
-                tv.superawesome.sdk.publisher.SAVideoAd.enableCloseButton()
-                tv.superawesome.sdk.publisher.SAInterstitialAd.enableCloseButton()
-            }
-
-            CloseButtonState.Hidden -> tv.superawesome.sdk.publisher.SAVideoAd.disableCloseButton()
-        }
-    }
-
     private fun updateMessage(placementId: Int, event: SAEvent) {
         val originalMessage =binding. subtitleTextView.text
-        val message = "$originalMessage $placementId $event"
-        binding.subtitleTextView.text = message
-    }
-
-    private fun updateMessage(placementId: Int, event: tv.superawesome.sdk.publisher.SAEvent) {
-        val originalMessage = binding.subtitleTextView.text
         val message = "$originalMessage $placementId $event"
         binding.subtitleTextView.text = message
     }
