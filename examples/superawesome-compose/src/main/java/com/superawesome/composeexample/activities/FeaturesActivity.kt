18096f49bf3306734c4d306d741b374f337a6a1f
Matheus Fernando Finatti <matheus.finatti@superawesome.com>
2023-09-08 14:24:48 +0100

    chore(common): create wrapper around the new AA initializer
    - Also removed it from the 'sdk' package

--- examples/superawesome-compose/src/main/java/com/superawesome/composeexample/activities/FeaturesActivity.kt
+++ examples/superawesome-compose/src/main/java/com/superawesome/composeexample/activities/FeaturesActivity.kt
@@ -25,7 +25,7 @@ import com.superawesome.composeexample.models.FeatureItem
 import com.superawesome.composeexample.models.PlacementItem
 import com.superawesome.composeexample.ui.theme.SAMobileSDKAndroidTheme
 import com.superawesome.composeexample.views.FeaturesList
-import tv.superawesome.sdk.publisher.sdk.AwesomeAds
+import tv.superawesome.sdk.publisher.AwesomeAds
 
 class FeaturesActivity : ComponentActivity() {
 
