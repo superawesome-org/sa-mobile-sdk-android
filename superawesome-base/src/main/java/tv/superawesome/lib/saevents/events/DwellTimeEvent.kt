4ad70c88b0ac0a97f0ed77e88d83a0d366622e23
Tom O'Rourke <30452349+thomas-james-1986@users.noreply.github.com>
2023-09-15 15:16:27 +0100

    Update superawesome-base/src/main/java/tv/superawesome/lib/saevents/events/DwellTimeEvent.kt
    Co-authored-by: Myles Eynon <me2k@users.noreply.github.com>

--- superawesome-base/src/main/java/tv/superawesome/lib/saevents/events/DwellTimeEvent.kt
+++ superawesome-base/src/main/java/tv/superawesome/lib/saevents/events/DwellTimeEvent.kt
@@ -31,7 +31,7 @@ class DwellTimeEvent(
                 "ct", session.connectionType.ordinal,
                 "sdkVersion", session.version,
                 "rnd", session.cachebuster,
-                "ad_request_id", ad.adRequestId
+                "adRequestId", ad.adRequestId
             )
         } catch (e: Exception) {
             JSONObject()
