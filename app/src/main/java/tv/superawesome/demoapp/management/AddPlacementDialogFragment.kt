81b356b933b5bede084f8fa614b734e89d25ba16
Tom O'Rourke <thomasjamesorourke@hotmail.co.uk>
2023-09-19 13:25:47 +0100

    chore(): validation and building placement

--- app/src/main/java/tv/superawesome/demoapp/management/AddPlacementDialogFragment.kt
+++ app/src/main/java/tv/superawesome/demoapp/management/AddPlacementDialogFragment.kt
@@ -2,20 +2,23 @@ package tv.superawesome.demoapp.management
 
 import android.annotation.SuppressLint
 import android.app.Dialog
-import android.content.DialogInterface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.Window
+import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.DialogFragment
 import tv.superawesome.demoapp.R
 import tv.superawesome.demoapp.databinding.FragmentAddPlacementBinding
+import tv.superawesome.demoapp.model.PlacementItem
 
 class AddPlacementDialogFragment : DialogFragment() {
-    var onDismissListener: (() -> Unit)? = null
+    var onSubmitListener: ((PlacementItem) -> Unit)? = null
 
     private var _binding: FragmentAddPlacementBinding? = null
+    private var isPlacementNameFieldValid = false
+    private var isPlacementIdFieldValid = false
     private val binding get() = _binding!!
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
@@ -45,13 +48,50 @@ class AddPlacementDialogFragment : DialogFragment() {
         super.onViewCreated(view, savedInstanceState)
         view.contentDescription = "AddPlacementView"
 
+        binding.addAdButton.setOnClickListener {
+            val placementName = binding.placementNameInput.text
+            val placementId = binding.placementIdInput.text
+            val lineItemId = binding.lineItemIdIdInput.text
+            val creativeId = binding.creativeIdIdInput.text
+
+            val placementItem = PlacementItem()
+
+            if(placementName.isNotBlank()) {
+                placementItem.name = placementName.toString()
+            }
+
+            if(placementId.isNotBlank()) {
+                placementItem.placementId = placementId.toString().toInt()
+            }
+
+            if(lineItemId.isNotBlank()) {
+                placementItem.lineItemId = lineItemId.toString().toInt()
+            }
+
+            if(creativeId.isNotBlank()) {
+                placementItem.creativeId = creativeId.toString().toInt()
+            }
+
+            onSubmitListener?.invoke(placementItem)
+            dismiss()
+        }
+
+        binding.placementNameInput.doAfterTextChanged {
+            isPlacementNameFieldValid = binding.placementNameInput.text.isNotBlank()
+            checkRequiredFields()
+        }
+
+        binding.placementIdInput.doAfterTextChanged {
+            isPlacementIdFieldValid = binding.placementIdInput.text.isNotBlank()
+            checkRequiredFields()
+        }
+
         binding.closeButton.setOnClickListener {
             dismiss()
         }
     }
 
-    override fun onDismiss(dialog: DialogInterface) {
-        super.onDismiss(dialog)
-        onDismissListener?.invoke()
+    private fun checkRequiredFields() {
+        binding.addAdButton.isEnabled = isPlacementNameFieldValid && isPlacementIdFieldValid
     }
 }
