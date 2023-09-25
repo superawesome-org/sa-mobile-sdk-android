1454a4d9af474ddd84ccbfc07f3c1e056e31abf5
Matheus Fernando Finatti <matheus.finatti@superawesome.com>
2023-09-25 11:09:21 +0100

    fix(events): Reduce dwell time event tick to every 5 seconds

--- superawesome-base/src/main/java/tv/superawesome/lib/saevents/events/DwellTimeEvent.kt
+++ superawesome-base/src/main/java/tv/superawesome/lib/saevents/events/DwellTimeEvent.kt
@@ -4,6 +4,7 @@ import org.json.JSONObject
 import tv.superawesome.lib.sajsonparser.SAJsonParser
 import tv.superawesome.lib.samodelspace.saad.SAAd
 import tv.superawesome.lib.sasession.session.ISASession
+import tv.superawesome.lib.sautils.SAUtils
 import java.util.concurrent.Executor
 
 class DwellTimeEvent(
@@ -20,6 +21,7 @@ class DwellTimeEvent(
         return "/event"
     }
 
+    @Suppress("MagicNumber")
     override fun getQuery(): JSONObject? {
         return try {
             SAJsonParser.newObject(
@@ -33,6 +35,13 @@ class DwellTimeEvent(
                 "rnd", session.cachebuster,
                 "adRequestId", ad.adRequestId,
                 "openRtbPartnerId", ad.openRtbPartnerId,
+                "data", SAUtils.encodeDictAsJsonDict(SAJsonParser.newObject(
+                    "creative", ad.creative.id,
+                    "line_item", ad.lineItemId,
+                    "placement", ad.placementId,
+                    "type", "viewTime",
+                    "value", 5,
+                )),
             )
         } catch (e: Exception) {
             JSONObject()
