df8683893d40301b82923790bbd456d1a3e70dee
Matheus Fernando Finatti <matheus.finatti@superawesome.com>
2023-09-26 10:10:15 +0100

    chore(events): Add dwell time event to banners and interstitials (common)

--- superawesome-common/internal/build.gradle.kts
+++ superawesome-common/internal/build.gradle.kts
@@ -27,6 +27,7 @@ dependencies {
     implementation(libs.kotlinx.coroutines.core)
     implementation(libs.kotlinx.coroutines.android)
 
+    implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.koin.android)
     implementation(libs.retrofit)
     implementation(libs.okhttp.logging.interceptor)
