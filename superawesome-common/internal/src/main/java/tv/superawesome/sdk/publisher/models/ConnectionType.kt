979a0f68607195c9ad5a239ec3e7674e12f192b4
Myles Eynon <me2k@users.noreply.github.com>
2023-10-02 16:24:45 +0100

    fix(ConnectionType): Added 5g connection type to common. (#450)

--- superawesome-common/internal/src/main/java/tv/superawesome/sdk/publisher/models/ConnectionType.kt
+++ superawesome-common/internal/src/main/java/tv/superawesome/sdk/publisher/models/ConnectionType.kt
@@ -1,12 +1,12 @@
 package tv.superawesome.sdk.publisher.models
 
 enum class ConnectionType {
-    Unknown, Ethernet, Wifi, CellularUnknown, Cellular2g, Cellular3g, Cellular4g;
+    Unknown, Ethernet, Wifi, CellularUnknown, Cellular2g, Cellular3g, Cellular4g, Cellular5g;
 
     fun findQuality(): ConnectionQuality {
         return when (this) {
             CellularUnknown, Cellular2g -> ConnectionQuality.Minimum
-            Ethernet, Cellular4g, Wifi, Unknown -> ConnectionQuality.Maximum
+            Ethernet, Cellular4g, Cellular5g, Wifi, Unknown -> ConnectionQuality.Maximum
             else -> ConnectionQuality.Medium
         }
     }
