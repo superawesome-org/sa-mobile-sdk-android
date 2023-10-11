69632e50a86d12051407162f7acb295afb90fb7d
Tom O'Rourke <30452349+thomas-james-1986@users.noreply.github.com>
2023-10-11 12:59:25 +0100

    Feat/aasdk 568 add unit tests for common models module (#460)
    * chore(): Unit tests for common models WIP
    
    * chore(): unit test for merging VastAds
    
    * chore(): Added UrlFileItem unit test
    
    * chore(): cleanup
    
    * chore(): cleanup
    
    * chore(): cleanup
    
    * chore(): cleanup
    
    * chore(): excluding serializers from jacoco test reports
    
    * chore(): test naming
    
    * chore(): removed unnecessary runTest calls

---
+++ superawesome-common/internal/src/test/java/tv/superawesome/sdk/publisher/models/CreativeModelsTest.kt
@@ -0,0 +1,29 @@
+package tv.superawesome.sdk.publisher.models
+
+import org.junit.Assert
+import org.junit.Test
+
+class CreativeModelsTest {
+
+    @Test
+    fun `CreativeReferral toMap contains expected values`() {
+        // Given
+        val sut = CreativeReferral(
+            utmSource = 1,
+            utmCampaign = 2,
+            utmTerm = 3,
+            utmContent = 4,
+            utmMedium = 5
+        )
+
+        // When
+        val map = sut.toMap()
+
+        // Then
+        Assert.assertEquals(map["utm_source"], sut.utmSource)
+        Assert.assertEquals(map["utm_campaign"], sut.utmCampaign)
+        Assert.assertEquals(map["utm_term"], sut.utmTerm)
+        Assert.assertEquals(map["utm_content"], sut.utmContent)
+        Assert.assertEquals(map["utm_medium"], sut.utmMedium)
+    }
+}
