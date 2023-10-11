9417ea3081864c458d6d03d215606a47f256e693
Myles Eynon <me2k@users.noreply.github.com>
2023-10-11 15:07:42 +0100

    chore(Libraries): Downgraded OKHttp to 3.12.13, Retrofit to 2.6.4 and added OKIO to provide API19 support. (#459)
    * chore(Libraries): Downgraded OKHttp to 3.12.13, Retrofit to 2.6.4 and added OKIO to provide API19 support.
    
    * fixed detekt errors
    
    * Converted common networking calls to use runCatching.
    
    * removed unrequired optional

--- app/src/main/java/tv/superawesome/demoapp/repository/FeaturesRepository.kt
+++ app/src/main/java/tv/superawesome/demoapp/repository/FeaturesRepository.kt
@@ -2,25 +2,36 @@ package tv.superawesome.demoapp.repository
 
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
+import okhttp3.ConnectionSpec
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import okhttp3.TlsVersion
 import tv.superawesome.demoapp.model.FeatureItem
 import tv.superawesome.demoapp.model.Features
 
-private val okHttpClient = OkHttpClient.Builder().build()
-private const val FEATURES_JSON_URL = "https://aa-sdk.s3.eu-west-1.amazonaws.com/placements.json"
-
-class FeaturesRepository {
-
+class FeaturesRepository(
+    private val connectionSpec: ConnectionSpec = ConnectionSpec.Builder(ConnectionSpec.MODERN_TLS)
+        .tlsVersions(TlsVersion.TLS_1_2)
+        .build(),
+    private val okHttpClient: OkHttpClient = OkHttpClient
+        .Builder()
+        .connectionSpecs(listOf(connectionSpec))
+        .build(),
+) {
     fun fetchAllFeatures(): DataResult<List<FeatureItem>> {
         val request = Request.Builder().url(FEATURES_JSON_URL).build()
         val response = okHttpClient.newCall(request).execute()
 
         return if (response.isSuccessful) {
-            val features = Json.decodeFromString<Features>(response.body?.string() ?: "")
+            val features = Json.decodeFromString<Features>(response.body()?.string() ?: "")
             DataResult.Success(features.features)
         } else {
-            DataResult.Failure(Error("Could not GET data from $FEATURES_JSON_URL"))
+            DataResult.Failure(Error("Could not GET data from ${FEATURES_JSON_URL}"))
         }
+    }
+
+    companion object {
+        private const val FEATURES_JSON_URL =
+            "https://aa-sdk.s3.eu-west-1.amazonaws.com/placements.json"
     }
 }
