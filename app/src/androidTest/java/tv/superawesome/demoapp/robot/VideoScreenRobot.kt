cc56727cac214723ee5b4424f31a47047b3c0737
Matheus Fernando Finatti <matheus.finatti@superawesome.com>
2023-11-10 15:09:35 +0000

    chore(ui-test): test video mute on start

--- app/src/androidTest/java/tv/superawesome/demoapp/robot/VideoScreenRobot.kt
+++ app/src/androidTest/java/tv/superawesome/demoapp/robot/VideoScreenRobot.kt
@@ -2,8 +2,11 @@ package tv.superawesome.demoapp.robot
 
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withContentDescription
+import androidx.test.espresso.matcher.ViewMatchers.withTagValue
+import org.hamcrest.Matchers.equalTo
 import tv.superawesome.demoapp.util.ViewTester
 import tv.superawesome.demoapp.util.isVisible
 import tv.superawesome.demoapp.util.waitUntil
@@ -44,6 +47,25 @@ class VideoScreenRobot : BaseRobot() {
     fun tapOnClose() {
         onView(withContentDescription("Close"))
             .perform(click())
+    }
+
+    fun tapOnVolume() {
+        onView(withContentDescription("Volume"))
+            .perform(click())
+    }
+
+    fun checkVideoIsMuted() {
+        onView(withContentDescription("Volume"))
+            .check(
+                matches(withTagValue(equalTo("MUTED")))
+            )
+    }
+
+    fun checkVideoIsUnmuted() {
+        onView(withContentDescription("Volume"))
+            .check(
+                matches(withTagValue(equalTo("UNMUTED")))
+            )
     }
 }
 
