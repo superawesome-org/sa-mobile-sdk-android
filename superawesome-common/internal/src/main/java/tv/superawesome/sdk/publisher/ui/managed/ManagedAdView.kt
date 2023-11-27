888e5ad1997a9acab8f455edb48bb9865e942061
Tom O'Rourke <thomasjamesorourke@hotmail.co.uk>
2023-11-27 17:53:58 +0000

    chore(): removed log

--- superawesome-common/internal/src/main/java/tv/superawesome/sdk/publisher/ui/managed/ManagedAdView.kt
+++ superawesome-common/internal/src/main/java/tv/superawesome/sdk/publisher/ui/managed/ManagedAdView.kt
@@ -8,7 +8,6 @@ import android.graphics.Color
 import android.os.Bundle
 import android.os.Parcelable
 import android.util.AttributeSet
-import android.util.Log
 import android.view.ViewGroup
 import android.webkit.WebView
 import android.widget.FrameLayout
@@ -194,7 +193,6 @@ public class ManagedAdView @JvmOverloads constructor(
                 controller.listener?.onEvent(placementId, SAEvent.adFailedToShow)
             }
             override fun webViewOnClick(url: String) {
-                Log.d("VPAID CLICK", url)
                 clickThrottler.onClick { controller.handleAdClick(url, context) }
             }
         }
