db7798804567d2096ab499ad17d5146c52bd2bec
Matheus Fernando Finatti <matheus.finatti@superawesome.com>
2024-01-17 16:34:10 -0300

    add UI test for inline vast

--- app/src/androidTest/java/tv/superawesome/demoapp/model/TestData.kt
+++ app/src/androidTest/java/tv/superawesome/demoapp/model/TestData.kt
@@ -204,5 +204,9 @@ data class TestData(
             creativeId = "503038",
             fileName = "interstitial_standard_success.json",
         )
+        val vastInline = TestData(
+            placementId = "10010",
+            fileName = "video_vast_inline.json"
+        )
     }
 }
