31b0d654d62eab14b4b11ade99fe1ae5672d6869
Myles Eynon <me2k@users.noreply.github.com>
2024-04-10 18:04:01 +0100

    chore(ironSource): Updated ironSource to v8.0.0 and SuperAwesomeAdapter to v4.1.9 (#566)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     defaultConfig {
         applicationId = "tv.superawesome.demoapp"
         versionCode = 1
-        versionName = "9.2.3"
+        versionName = "9.3.3"
 
         testInstrumentationRunner = "tv.superawesome.demoapp.runner.UITestRunner"
         testInstrumentationRunnerArguments["clearPackageData"] = "true"
